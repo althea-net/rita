0c4d05fb689ea70ca4d6bc5dfd4942f772715674
jkilpatr <jkilpatr@redhat.com>
2017-09-17 09:51:03 -0400

    Added tests and parsing
    So I got as far as trying to write the validation function before I
    realized that to really trust RTT as a metric we needed to break out from
    hop to hop only advertisement and pass it along as it's own seperate metric.
    
    So off to make that modification to babel now.

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@ use std::io;
 use std::io::{Read,Write};
 use std::net::TcpStream;
 extern crate mockstream;
-extern crate ascii;
 use std::str;
 use mockstream::SharedMockStream;
+use std::collections::VecDeque;
 
 // From rust_mockstream MIT
-enum NetStream {
+pub enum NetStream {
 	Mocked(SharedMockStream),
 	Tcp(TcpStream)
 }
@@ -38,14 +38,79 @@ impl io::Write for NetStream {
 }
 // end mockstream code
 
-struct Babel {
-    stream: NetStream
+// If a function doesn't modify the state of the Babel object
+// we don't want to place it as a member function.
+fn parse_babel_val(val: &str, line: &str) -> String {
+    let mut place = line.split(" ");
+    let mut entry = place.next();
+    assert_eq!(entry.is_some(), true);
+    while !entry.unwrap().to_string().contains(val) {
+        entry = place.next();
+        assert_eq!(entry.is_some(), true);
+    }
+    place.next().unwrap().to_string()
+}
+
+// Grabs route reference that is valid so long as the routes vector continues to exist.
+// Should be fine for our usage. 
+fn get_route_by_prefix<'a>(prefix: &str, routes: &'a VecDeque<Route>) -> Option<&'a Route> {
+    let mut ret: Option<&Route> = None;
+    for route in routes.iter() {
+        if route.prefix.contains(prefix) {
+            ret = Some(route);
+            break;
+        }
+    }
+    ret
+}
+
+fn get_neigh_by_iface<'a>(iface: &str, neighs: &'a VecDeque<Neighbour>) -> Option<&'a Neighbour> {
+    let mut ret: Option<&Neighbour> = None;
+    for neigh in neighs.iter() {
+        if neigh.iface.contains(iface) {
+            ret = Some(neigh);
+            break;
+        }
+    }
+    ret
+}
+
+#[derive(Debug)]
+pub struct Route {
+    id: String,
+    iface: String,
+    xroute: bool,
+    installed: bool,
+    neigh_ip: String,
+    prefix: String,
+    metric: u16,
+    refmetric: u16,
+    price: u32
+
+}
+
+#[derive(Debug)]
+pub struct Neighbour {
+    id: String,
+    iface: String,
+    reach: u16,
+    txcost: u16,
+    rxcost: u16,
+    rtt: f32,
+    rttcost: u16,
+    cost: u16
+}
+
+pub struct Babel {
+    stream: NetStream,
+    verification_iface: String,
+    verification_endpoint: String
 }
 
 impl Babel {
 
     // Consumes the automated Preamble and validates configuration api version
-    fn start_connection(&mut self) -> bool {
+    pub fn start_connection(&mut self) -> bool {
         let preamble = self.read();
         // Note you have changed the config interface, bump to 1.1 in babel
         if preamble.contains("BABEL 1.0") && self.positive_termination(&preamble) {
@@ -58,21 +123,20 @@ impl Babel {
 
     // Safely closes the babel connection and terminates
     // the monitor thread
-    fn close_connection(&mut self) -> bool {
+    pub fn close_connection(&mut self) -> bool {
         true
     }
 
     //TODO write function to shrink these strings in memory
     fn read(&mut self) -> String {
         let mut data: [u8; 512] = [0;512];
-        self.stream.read(&mut data);
+        assert!(self.stream.read(&mut data).is_ok());
         let mut ret = str::from_utf8(&data).unwrap().to_string();
         // Messages may be long or get interupped, we must consume
         // until we hit a terminator
         loop {
-            self.stream.read(&mut data);
-            let mut message = &str::from_utf8(&data).unwrap().to_string();
-            println!("{:?}", message);
+            assert!(self.stream.read(&mut data).is_ok());
+            let message = &str::from_utf8(&data).unwrap().to_string();
             ret = ret + message;
             if self.contains_terminator(message) {
                 break;
@@ -81,7 +145,7 @@ impl Babel {
         ret
     }
 
-    fn contains_terminator(&self, message: &str) -> bool {
+    fn contains_terminator(&self, message: &String) -> bool {
         if message.contains("\nok\n") {
             true
         }
@@ -96,7 +160,7 @@ impl Babel {
         }
     }
 
-    fn positive_termination(&self, message: &str) -> bool {
+    fn positive_termination(&self, message: &String) -> bool {
         if message.contains("\nok\n") {
             true
         }
@@ -113,39 +177,178 @@ impl Babel {
         true
     }
 
-    fn parse_babel_val(&self, val: &'static str, line: &'static str) -> String {
-        let mut place = line.split(" ");
-        let mut entry = place.next();
-        while !entry.unwrap().to_string().contains(val) {
-            entry = place.next();
+    pub fn local_price(&mut self) -> u32 {
+        assert_eq!(self.write("dump\n"), true);
+        let table = self.read();
+        let table = table.split("\n");
+        let mut price = 0;
+        for entry in table {
+            if entry.contains("local price") {
+                let price_str = parse_babel_val("price", entry);
+                let price_o = price_str.parse::<u32>();
+                price = price_o.unwrap();
+                break;
+            }
         }
-        place.next().unwrap().to_string()
+        price
+    }
+
+
+    pub fn parse_neighs(&mut self) -> VecDeque<Neighbour> {
+        let mut vector: VecDeque<Neighbour> = VecDeque::with_capacity(5);
+        assert_eq!(self.write("dump\n"), true);
+        let table = self.read();
+        let table = table.split("\n");
+        for entry in table {
+            if entry.contains("add neighbour") {
+                let id = parse_babel_val("neighbour", entry);
+
+                let iface = parse_babel_val("if", entry);
+
+                let reach_str = parse_babel_val("reach", entry);
+                let reach_o = u16::from_str_radix(&reach_str, 16);
+                let reach = reach_o.unwrap();
+ 
+                let txcost_str = parse_babel_val("txcost", entry);
+                let txcost_o = txcost_str.parse::<u16>();
+                let txcost = txcost_o.unwrap();
+                 
+                let rxcost_str = parse_babel_val("rxcost", entry);
+                let rxcost_o = rxcost_str.parse::<u16>();
+                let rxcost = rxcost_o.unwrap();
+
+                let rtt_str = parse_babel_val("rtt", entry);
+                let rtt_o = rtt_str.parse::<f32>();
+                let rtt = rtt_o.unwrap();
+
+                let rttcost_str = parse_babel_val("rttcost", entry);
+                let rttcost_o = rttcost_str.parse::<u16>();
+                let rttcost = rttcost_o.unwrap();
+
+                let cost_str = parse_babel_val("cost", entry);
+                let cost_o = cost_str.parse::<u16>();
+                let cost = cost_o.unwrap();
+
+                let n = Neighbour {id,
+                                   iface,
+                                   reach,
+                                   txcost,
+                                   rxcost,
+                                   rtt,
+                                   rttcost,
+                                   cost};
+                vector.push_back(n);
+
+            }
+        }
+        vector
+    }
+   
+    
+    pub fn parse_routes(&mut self) -> VecDeque<Route> {
+        let mut vector: VecDeque<Route> = VecDeque::with_capacity(20);
+        assert_eq!(self.write("dump\n"), true);
+        let table = self.read();
+        let table = table.split("\n");
+        for entry in table {
+            if entry.contains("add route") {
+                let id = parse_babel_val("route", entry);
+
+                let iface = parse_babel_val("if", entry);
+
+                let xroute = false;
+
+                let mut installed = false;
+                if parse_babel_val("installed", entry).contains("yes") {
+                    installed = true;
+                }
+                
+                let neigh_ip = parse_babel_val("via", entry);
+
+                let prefix = parse_babel_val("prefix", entry);
+
+                let metric_str = parse_babel_val("metric", entry);
+                let metric_o = metric_str.parse::<u16>();
+                let metric = metric_o.unwrap();
+                
+                let refmetric_str = parse_babel_val("refmetric", entry);
+                let refmetric_o = refmetric_str.parse::<u16>();
+                let refmetric = refmetric_o.unwrap();
+                 
+                let price_str = parse_babel_val("price", entry);
+                let price_o = price_str.parse::<u32>();
+                let price = price_o.unwrap();
+
+                let n = Route {id,
+                               iface,
+                               xroute,
+                               installed,
+                               neigh_ip,
+                               prefix,
+                               metric,
+                               refmetric,
+                               price};
+                vector.push_back(n);
+
+            }
+            else if entry.contains("add xroute") {
+
+                let id = "XROUTE".to_string();
+
+                let iface = "XROUTE".to_string();
+
+                let xroute = true;
+
+                let installed = true;
+
+                let neigh_ip = "XROUTE".to_string();
+
+                let prefix = parse_babel_val("prefix", entry);
+
+                let metric_str = parse_babel_val("metric", entry);
+                let metric_o = metric_str.parse::<u16>();
+                let metric = metric_o.unwrap();
+
+                let refmetric = 0;
+
+                let price = 0;
+                
+                let n = Route {id,
+                               iface,
+                               xroute,
+                               installed,
+                               neigh_ip,
+                               prefix,
+                               metric,
+                               refmetric,
+                               price};
+                vector.push_back(n);
+
+            }
+        }
+        vector
+    }
+
+    pub fn fraud_check(&mut self) -> bool {
+        let neighs = self.parse_neighs();
+        let routes = self.parse_routes();
+
+        let gateway_o = get_neigh_by_iface(&self.verification_iface, &neighs);
+        let route_o = get_route_by_prefix(&self.verification_endpoint, &routes);
+        assert!(gateway_o.is_some());
+        assert!(route_o.is_some());
+
+        let route = route_o.unwrap();
+        let gateway = gateway_o.unwrap();
+
+        //insert realization that modifications to babel are required here
+
+
+        false
     }
 
 }
 
-struct Route {
-    xroute: bool,
-    installed: bool,
-    id: String,
-    neigh_id: String,
-    ip: String,
-    metric: u16,
-    price: u32,
-    refmetric: u16
-
-}
-
-struct Neighbour {
-    id: String,
-    iface: String,
-    reach: u16,
-    txcost: u16,
-    rxcost: u16,
-    rtt: f32,
-    rttcost: u16,
-    cost: u16
-}
 
 #[cfg(test)]
 mod tests {
@@ -168,9 +371,24 @@ mod tests {
                                   wlan0\nadd route 14f0548 prefix 10.28.244.138/32 from 0.0.0.0/0 installed yes id \
                                   ba:27:eb:ff:fe:d1:3e:ba metric 958 price 2048 refmetric 0 via fe80::e914:2335:a76:bda3 if \
                                   wlan0\nok\n\u{0}\u{0}";
+
     static PREAMBLE: &'static str = "BABEL 1.0\nversion babeld-1.8.0-24-g6335378\nhost raspberrypi\nmy-id \
                                     ba:27:eb:ff:fe:09:06:dd\nok\n\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\
                                     \u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}\u{0}";
+
+    static XROUTE_LINE: &'static str = "add xroute 10.28.119.131/32-::/0 prefix 10.28.119.131/32 from ::/0 metric 0";
+
+    static ROUTE_LINE: &'static str = "add route 14f06d8 prefix 10.28.20.151/32 from 0.0.0.0/0 installed yes id \
+                                       ba:27:eb:ff:fe:c1:2d:d5 metric 1306 price 4008 refmetric 0 via \
+                                       fe80::e9d0:498f:6c61:be29 if wlan0";
+
+    static NEIGH_LINE: &'static str = "add neighbour 14f05f0 address fe80::e9d0:498f:6c61:be29 if wlan0 reach ffff rxcost \
+                                       256 txcost 256 rtt 29.264 rttcost 1050 cost 1306";
+
+
+    static IFACE_LINE: &'static str = "add interface wlan0 up true ipv6 fe80::1a8b:ec1:8542:1bd8 ipv4 10.28.119.131";
+
+    static PRICE_LINE: &'static str = "local price 1024";
 
     /*fn real_babel_basic() {
         let mut b1 = Babel {stream: NetStream::Tcp(TcpStream::connect("::1:8080").unwrap())};
@@ -182,16 +400,79 @@ mod tests {
     #[test]
     fn mock_connect() {
         let mut s = SharedMockStream::new();
-        let mut b1 = Babel {stream: NetStream::Mocked(s.clone())};
+        let mut b1 = Babel {stream: NetStream::Mocked(s.clone()), 
+                            verification_iface: "wg0".to_string(), 
+                            verification_endpoint: "10.28.7.7".to_string()};
         s.push_bytes_to_read(PREAMBLE.as_bytes());
         assert_eq!(b1.start_connection(), true);
+        assert_eq!(b1.close_connection(), true);
     }
 
     #[test]
     fn mock_dump() {
         let mut s = SharedMockStream::new();
-        let mut b1 = Babel {stream: NetStream::Mocked(s.clone())};
+        let mut b1 = Babel {stream: NetStream::Mocked(s.clone()), 
+                            verification_iface: "wg0".to_string(), 
+                            verification_endpoint: "10.28.7.7".to_string()};
         s.push_bytes_to_read(TABLE.as_bytes());
         assert_eq!(b1.write("dump\n"), true);
+    }
+
+    #[test]
+    fn line_parse() {
+        assert_eq!(parse_babel_val("metric", XROUTE_LINE), "0");
+        assert_eq!(parse_babel_val("prefix", XROUTE_LINE), "10.28.119.131/32");
+        assert_eq!(parse_babel_val("route", ROUTE_LINE), "14f06d8");
+        assert_eq!(parse_babel_val("if", ROUTE_LINE), "wlan0");
+        assert_eq!(parse_babel_val("via", ROUTE_LINE), "fe80::e9d0:498f:6c61:be29");
+        assert_eq!(parse_babel_val("reach", NEIGH_LINE), "ffff");
+        assert_eq!(parse_babel_val("rxcost", NEIGH_LINE), "256");
+        assert_eq!(parse_babel_val("rtt", NEIGH_LINE), "29.264");
+        assert_eq!(parse_babel_val("interface", IFACE_LINE), "wlan0");
+        assert_eq!(parse_babel_val("ipv4", IFACE_LINE), "10.28.119.131");
+        assert_eq!(parse_babel_val("price", PRICE_LINE), "1024");
+    }
+
+    #[test]
+    fn neigh_parse() {
+        let mut s = SharedMockStream::new();
+        let mut b1 = Babel {stream: NetStream::Mocked(s.clone()), 
+                            verification_iface: "wg0".to_string(), 
+                            verification_endpoint: "10.28.7.7".to_string()};
+        s.push_bytes_to_read(TABLE.as_bytes());
+        assert_eq!(b1.write("dump\n"), true);
+        let neighs = b1.parse_neighs();
+        let neigh = neighs.get(0);
+        assert!(neigh.is_some());
+        let neigh = neigh.unwrap();
+        assert_eq!(neighs.len(), 4);
+        assert_eq!(neigh.id, "14f19a8");
+    }
+
+    #[test]
+    fn route_parse() {
+        let mut s = SharedMockStream::new();
+        let mut b1 = Babel {stream: NetStream::Mocked(s.clone()), 
+                            verification_iface: "wg0".to_string(), 
+                            verification_endpoint: "10.28.7.7".to_string()};
+        s.push_bytes_to_read(TABLE.as_bytes());
+        assert_eq!(b1.write("dump\n"), true);
+        let routes = b1.parse_routes();
+        let route = routes.get(0);
+        assert!(route.is_some());
+        let route = route.unwrap();
+        assert_eq!(routes.len(), 5);
+        assert_eq!(route.price, 0);
+    }
+
+    #[test]
+    fn local_price_parse() { 
+        let mut s = SharedMockStream::new();
+        let mut b1 = Babel {stream: NetStream::Mocked(s.clone()), 
+                            verification_iface: "wg0".to_string(), 
+                            verification_endpoint: "10.28.7.7".to_string()};
+        s.push_bytes_to_read(TABLE.as_bytes());
+        assert_eq!(b1.write("dump\n"), true);
+        assert_eq!(b1.local_price(), 1024);
     }
 }
