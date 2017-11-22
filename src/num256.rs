c00b3cefecc210455f69f0e42c6cbc28edb770b2
Jehan <jehan.tremback@gmail.com>
2017-11-21 21:09:45 -0800

    seems like the bignums are ok

--- src/num256.rs
+++ src/num256.rs
@@ -16,17 +16,21 @@ impl Deref for Uint256 {
 impl Add for Uint256 {
   type Output = Uint256;
   fn add(self, v: Uint256) -> Uint256 {
-    Uint256(self.0 + v.0)
+    let num = self.0 + v.0;
+    if num.bits() > 256 {
+      panic!("overflow");
+    }
+    Uint256(num)
   }
 }
 
 impl CheckedAdd for Uint256 {
   fn checked_add(&self, v: &Uint256) -> Option<Uint256> {
-    let num = self.clone() + v.clone();
+    let num = self.0.clone() + v.0.clone();
     if num.bits() > 256 {
       return None;
     }
-    Some(num)
+    Some(Uint256(num))
   }
 }
 
@@ -61,34 +65,44 @@ impl Deref for Int256 {
 impl Add for Int256 {
   type Output = Int256;
   fn add(self, v: Int256) -> Int256 {
-    Int256(self.0 + v.0)
+    let num = self.0 + v.0;
+    if num.bits() > 255 {
+      panic!("overflow");
+    }
+    Int256(num)
   }
 }
 
 impl CheckedAdd for Int256 {
   fn checked_add(&self, v: &Int256) -> Option<Int256> {
-    let num = self.clone() + v.clone();
-    if num.bits() > 256 {
+    // drop down to wrapped bigint to stop from panicing in fn above
+    let num = self.0.clone() + v.0.clone();
+    if num.bits() > 255 {
       return None;
     }
-    Some(num)
+    Some(Int256(num))
   }
 }
 
 impl Sub for Int256 {
   type Output = Int256;
   fn sub(self, v: Int256) -> Int256 {
-    Int256(self.0 - v.0)
+    let num = self.0 - v.0;
+    if num.bits() > 255 {
+      panic!("overflow");
+    }
+    Int256(num)
   }
 }
 
 impl CheckedSub for Int256 {
   fn checked_sub(&self, v: &Int256) -> Option<Int256> {
-    if self.0 < v.0 {
+    // drop down to wrapped bigint to stop from panicing in fn above
+    let num = self.0.clone() - v.0.clone();
+    if num.bits() > 255 {
       return None;
     }
-    let num = self.clone() - v.clone();
-    Some(num)
+    Some(Int256(num))
   }
 }
 
@@ -98,7 +112,6 @@ mod tests {
   use num256::{BigInt, BigUint, Int256, Uint256};
   use num::pow::pow;
   use num::traits::ops::checked::{CheckedAdd, CheckedSub};
-  use std::ops::Sub;
   use num::traits::cast::ToPrimitive;
   #[test]
   fn test_uint256() {
@@ -107,22 +120,30 @@ mod tests {
     assert!(
       biggest
         .checked_add(&Uint256(BigUint::from(1 as u32)))
-        .is_none()
+        .is_none(),
+      "should return None adding 1 to biggest"
     );
 
     assert!(
       biggest
         .checked_add(&Uint256(BigUint::from(0 as u32)))
-        .is_some()
+        .is_some(),
+      "should return None adding 0 to biggest"
     );
 
-    assert!(&Uint256(BigUint::from(1 as u32))
-      .checked_sub(&Uint256(BigUint::from(2 as u32)))
-      .is_none());
+    assert!(
+      &Uint256(BigUint::from(1 as u32))
+        .checked_sub(&Uint256(BigUint::from(2 as u32)))
+        .is_none(),
+      "should return None if RHS is larger than LHS"
+    );
 
-    assert!(&Uint256(BigUint::from(1 as u32))
-      .checked_sub(&Uint256(BigUint::from(1 as u32)))
-      .is_some());
+    assert!(
+      &Uint256(BigUint::from(1 as u32))
+        .checked_sub(&Uint256(BigUint::from(1 as u32)))
+        .is_some(),
+      "should return Some if RHS is not larger than LHS"
+    );
 
     let num = &Uint256(BigUint::from(1 as u32))
       .checked_sub(&Uint256(BigUint::from(1 as u32)))
@@ -130,7 +151,7 @@ mod tests {
       .to_u32()
       .unwrap();
 
-    assert_eq!(*num, 0);
+    assert_eq!(*num, 0, "1 - 1 should = 0");
 
     let num2 = &Uint256(BigUint::from(346 as u32))
       .checked_sub(&Uint256(BigUint::from(23 as u32)))
@@ -138,58 +159,38 @@ mod tests {
       .to_u32()
       .unwrap();
 
-    assert_eq!(*num2, 323);
+    assert_eq!(*num2, 323, "346 - 23 should = 323");
   }
 
   #[test]
   fn test_int256() {
-    let biggest = Int256(pow(BigInt::from(2 as u32), 255) - BigInt::from(1 as u32));
-    let smallest = Int256(pow(BigInt::from(-2 as i32), 255));
+    let biggest = Int256(pow(BigInt::from(2), 255) - BigInt::from(1));
+    let smallest = Int256(pow(BigInt::from(-2), 255) + BigInt::from(1));
 
-    let bigger = biggest
-      .checked_add(&Int256(BigInt::from(100000 as u32)))
-      .unwrap();
-
-    // This is where i'm working
-    println!(
-      "biggest {:?}, bits {:?}",
-      &biggest.to_str_radix(10),
-      &biggest.bits()
+    assert!(
+      biggest.checked_add(&Int256(BigInt::from(1))).is_none(),
+      "should return None adding 1 to biggest"
     );
-    println!(
-      "bigger {:?}, bits {:?}",
-      &bigger.to_str_radix(10),
-      &biggest.bits()
+    assert!(
+      biggest.checked_add(&Int256(BigInt::from(0))).is_some(),
+      "should return Some adding 1 to biggest"
     );
 
     assert!(
-      biggest
-        .checked_add(&Int256(BigInt::from(2 as u32)))
-        .is_none()
+      smallest.checked_sub(&Int256(BigInt::from(1))).is_none(),
+      "should return None subtracting 1 from smallest"
     );
     assert!(
-      biggest
-        .checked_add(&Int256(BigInt::from(0 as u32)))
-        .is_some()
+      smallest.checked_sub(&Int256(BigInt::from(0))).is_some(),
+      "should return Some subtracting 0 from smallest"
     );
 
-    assert!(
-      smallest
-        .checked_sub(&Int256(BigInt::from(1 as u32)))
-        .is_none()
-    );
-    assert!(
-      smallest
-        .checked_sub(&Int256(BigInt::from(0 as u32)))
-        .is_some()
-    );
-
-    let num = &Int256(BigInt::from(1 as u32))
-      .checked_sub(&Int256(BigInt::from(1 as u32)))
+    let num = &Int256(BigInt::from(345))
+      .checked_sub(&Int256(BigInt::from(44)))
       .unwrap()
       .to_u32()
       .unwrap();
 
-    assert_eq!(*num, 0);
+    assert_eq!(*num, 301, "345 - 44 should = 301");
   }
 }
