96aee50c3ea58f36e4d5e4d4d312bb1c77869d78
Jehan <jehan.tremback@gmail.com>
2017-12-19 23:54:39 -0800

    added iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 #[macro_use]
 extern crate serde_derive;
-use rocksdb::DB;
+
+use rocksdb::{DBIterator, IteratorMode, DB, Direction};
+
 #[macro_use]
 extern crate derive_error;
 
 extern crate rocksdb;
 extern crate serde;
 extern crate serde_json;
+use std::marker::PhantomData;
 
 use serde::de::DeserializeOwned;
 use serde::Serialize;
@@ -43,18 +46,12 @@ pub enum Error {
 /// assert_eq!(dog, store.get(&dog.name).unwrap().unwrap());
 
 /// ```
-pub trait Storage {
+pub trait Storage<T: Serialize + DeserializeOwned, U: Serialize + DeserializeOwned, I: Iterator<Item = (T, U)>> {
     /// Get a value from the current key or None if it does not exist.
-    fn get<T: Serialize, U: Serialize + DeserializeOwned>(
-        &self,
-        id: &T,
-    ) -> Result<Option<U>, Error>;
+    fn get(&self, id: &T) -> Result<Option<U>, Error>;
     /// Insert a value under the given key. Will overwrite the existing value.
-    fn insert<T: Serialize, U: Serialize + DeserializeOwned>(
-        &self,
-        id: &T,
-        item: &U,
-    ) -> Result<(), Error>;
+    fn insert(&self, id: &T, item: &U) -> Result<(), Error>;
+    fn iter(&self) -> I;
 }
 
 /// RocksJsonStorage implements the Storage trait, storing data in RocksDB,
@@ -88,20 +85,35 @@ pub trait Storage {
 ///     assert_eq!(dog, store.get(&dog.name).unwrap().unwrap());
 /// }
 /// ```
-pub struct RocksJsonStorage {
+pub struct RocksJsonStorage<T: Serialize + DeserializeOwned, U: Serialize + DeserializeOwned> {
     db: DB,
     bucket: u8,
+    phantom1: PhantomData<T>,
+    phantom2: PhantomData<U>,
 }
 
-impl RocksJsonStorage {
-    pub fn new(bucket: u8, path: &str) -> RocksJsonStorage {
+impl<T, U> RocksJsonStorage<T, U>
+    where
+        T: Serialize + DeserializeOwned,
+        U: Serialize + DeserializeOwned {
+    pub fn new(bucket: u8, path: &str) -> RocksJsonStorage<T, U> {
         RocksJsonStorage {
             db: DB::open_default(path).unwrap(),
             bucket,
+            phantom1: PhantomData,
+            phantom2: PhantomData,
         }
     }
     fn my_bucket(&self) -> u8 {
         self.bucket
+    }
+    fn iter(&self) -> RocksJsonIterator<T, U> {
+        RocksJsonIterator {
+            bucket: self.bucket,
+            db_iter: self.db.iterator(IteratorMode::Start),
+            phantom1: PhantomData,
+            phantom2: PhantomData,
+        }
     }
 }
 
@@ -112,11 +124,11 @@ fn prefix_with_bucket(typ: u8, input: &[u8]) -> Vec<u8> {
     v
 }
 
-impl Storage for RocksJsonStorage {
-    fn get<T: Serialize, U: Serialize + DeserializeOwned>(
-        &self,
-        id: &T,
-    ) -> Result<Option<U>, Error> {
+impl<
+    T: Serialize + DeserializeOwned,
+    U: Serialize + DeserializeOwned,
+> Storage<T, U, RocksJsonIterator<T, U>> for RocksJsonStorage<T, U> {
+    fn get(&self, id: &T) -> Result<Option<U>, Error> {
         Ok(match self.db.get(&prefix_with_bucket(
             self.my_bucket(),
             &serde_json::to_vec(&id)?,
@@ -125,24 +137,58 @@ impl Storage for RocksJsonStorage {
             _ => None,
         })
     }
-    fn insert<T: Serialize, U: Serialize + DeserializeOwned>(
-        &self,
-        id: &T,
-        item: &U,
-    ) -> Result<(), Error> {
+    fn insert(&self, id: &T, item: &U) -> Result<(), Error> {
         Ok(self.db.put(
             &prefix_with_bucket(self.my_bucket(), &serde_json::to_vec(&id)?),
             &serde_json::to_vec(&item)?,
         )?)
     }
+    fn iter(&self) -> RocksJsonIterator<T, U> {
+        RocksJsonIterator {
+            db_iter: self.db.iterator(IteratorMode::From(&[self.bucket], Direction::Forward)),
+            bucket: self.bucket,
+            phantom1: PhantomData,
+            phantom2: PhantomData,
+        }
+    }
 }
 
+struct RocksJsonIterator<T: Serialize + DeserializeOwned, U: Serialize + DeserializeOwned> {
+    db_iter: DBIterator,
+    bucket: u8,
+    phantom1: PhantomData<T>,
+    phantom2: PhantomData<U>,
+}
+
+impl<T: Serialize + DeserializeOwned, U: Serialize + DeserializeOwned> Iterator
+    for RocksJsonIterator<T, U> {
+    type Item = (T, U);
+
+    fn next(&mut self) -> Option<(T, U)> {
+        match self.db_iter.next() {
+            Some(v) => {
+                let (bucket, key): (&[u8], &[u8]) = v.0.split_at(1);
+                if bucket[0] != self.bucket {
+                    None
+                } else {
+                    Some((
+                        serde_json::from_slice(key).unwrap(),
+                        serde_json::from_slice(&v.1).unwrap(),
+                    ))
+                }
+            },
+            None => None,
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashMap;
+
     enum Breeds {
-        Shiba = 1,
+        Shiba = 10,
         Rottweiler = 2,
     }
 
@@ -153,7 +199,7 @@ mod tests {
         age: u64,
     }
     #[test]
-    fn it_works() {
+    fn insert_and_get() {
         let store = RocksJsonStorage::new(Breeds::Shiba as u8, ".db");
         let dog = Dog {
             name: "franklin".to_string(),
@@ -163,5 +209,26 @@ mod tests {
 
         store.insert(&dog.name, &dog).unwrap();
         assert_eq!(dog, store.get(&dog.name).unwrap().unwrap());
+    }
+
+    #[test]
+    fn iterate() {
+        let store = RocksJsonStorage::new(Breeds::Shiba as u8, ".db2");
+        let dog1 = Dog {
+            name: "franklin".to_string(),
+            color: "brown".to_string(),
+            age: 3,
+        };
+        let dog2 = Dog {
+            name: "ro".to_string(),
+            color: "red".to_string(),
+            age: 5,
+        };
+        store.insert(&dog1.name, &dog1).unwrap();
+        store.insert(&dog2.name, &dog2).unwrap();
+
+        let map: HashMap<String, Dog> = store.iter().collect();
+
+        assert_eq!("{\"ro\": Dog { name: \"ro\", color: \"red\", age: 5 }, \"franklin\": Dog { name: \"franklin\", color: \"brown\", age: 3 }}", format!("{:?}", map));
     }
 }
