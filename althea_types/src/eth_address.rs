533737c21c779389ce531ac170588dc947750431
Jehan <jehan.tremback@gmail.com>
2017-12-31 13:32:42 -0800

    split althea_types into modules and get rid of array_serialization_derive for now

---
+++ althea_types/src/eth_address.rs
@@ -0,0 +1,68 @@
+use std::ops::Deref;
+use std::str::FromStr;
+use std::fmt;
+use hex;
+use serde;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::hash::{Hash, Hasher};
+
+#[derive(Copy, Clone)]
+pub struct EthAddress(pub [u8; 20]);
+
+impl Hash for EthAddress {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.hash(state);
+    }
+}
+
+impl PartialEq for EthAddress {
+    fn eq(&self, other: &EthAddress) -> bool {
+        self.0[..] == other.0[..]
+    }
+}
+impl Eq for EthAddress {}
+
+impl Deref for EthAddress {
+    type Target = [u8; 20 as usize];
+
+    fn deref(&self) -> &[u8; 20 as usize] {
+        &self.0
+    }
+}
+
+impl FromStr for EthAddress {
+    type Err = hex::FromHexError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        hex::decode(&s[2..]).map(|v| {
+            let mut arr = [0u8; 20];
+            arr.clone_from_slice(&v);
+            EthAddress(arr)
+        })
+    }
+}
+
+impl fmt::Debug for EthAddress {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "EthAddress {}", self.to_string())
+    }
+}
+
+impl fmt::Display for EthAddress {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "0x{}", hex::encode(self.0.as_ref()))
+    }
+}
+
+impl Serialize for EthAddress {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+impl<'de: 'a, 'a> Deserialize<'de> for EthAddress {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let s = <&str>::deserialize(deserializer)?;
+        s.parse().map_err(serde::de::Error::custom)
+    }
+}
--- althea_types/src/lib.rs
+++ althea_types/src/lib.rs
@@ -2,99 +2,19 @@ extern crate serde;
 extern crate serde_json;
 extern crate base64;
 extern crate hex;
-use std::ops::Deref;
-use std::error::Error;
-//e().map_err(|e| eprintln!("{}", e.description())).unwrap();
+
 #[macro_use]
 extern crate serde_derive;
 
-#[macro_use]
-extern crate array_serialization_derive;
+pub mod bytes_32;
+pub mod eth_address;
+pub mod eth_private_key;
+pub mod eth_signature;
 
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
-use std::str::FromStr;
-use std::fmt;
-
-#[derive(ArrayTupleDeref, ArrayTupleBase64, PartialEq)]
-pub struct Bytes32([u8; 32]);
-
-impl fmt::Debug for Bytes32 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Bytes32 {:?}", &&self.0[..])
-    }
-}
-
-#[derive(ArrayTupleDeref, Copy, Clone, Hash, Eq, PartialEq)]
-pub struct EthAddress([u8; 20]);
-
-impl FromStr for EthAddress {
-    type Err = hex::FromHexError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        hex::decode(&s[2..]).map(|v| {
-            let mut arr = [0u8; 20];
-            arr.clone_from_slice(&v);
-            EthAddress(arr)
-        })
-    }
-}
-
-impl fmt::Debug for EthAddress {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "EthAddress {}", self.to_string())
-    }
-}
-
-impl fmt::Display for EthAddress {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "0x{}", hex::encode(self.0.as_ref()))
-    }
-}
-
-impl Serialize for EthAddress {
-    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_str(&self.to_string())
-    }
-}
-
-impl<'de: 'a, 'a> Deserialize<'de> for EthAddress {
-    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let s = <&str>::deserialize(deserializer)?;
-        s.parse().map_err(serde::de::Error::custom)
-    }
-}
-
-
-#[derive(ArrayTupleDeref, ArrayTupleBase64)]
-pub struct EthSignature([u8; 65]);
-
-impl PartialEq for EthSignature {
-    fn eq(&self, other: &EthSignature) -> bool {
-        self.0[..] == other.0[..]
-    }
-}
-
-impl fmt::Debug for EthSignature {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "EthSignature {:?}", &&self.0[..])
-    }
-}
-
-#[derive(ArrayTupleDeref, ArrayTupleBase64)]
-pub struct EthPrivateKey([u8; 64]);
-
-impl PartialEq for EthPrivateKey {
-    fn eq(&self, other: &EthPrivateKey) -> bool {
-        self.0[..] == other.0[..]
-    }
-}
-
-
-impl fmt::Debug for EthPrivateKey {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "EthPrivateKey {:?}", &&self.0[..])
-    }
-}
+pub use bytes_32::Bytes32;
+pub use eth_address::EthAddress;
+pub use eth_private_key::EthPrivateKey;
+pub use eth_signature::EthSignature;
 
 #[cfg(test)]
 mod tests {
@@ -120,7 +40,7 @@ mod tests {
         // Serialize it to a JSON string.
         let j = serde_json::to_string(&my_struct).unwrap();
 
-        let s = "{\"addr\":\"0x0707070707070707070707070707070707070707\",\"sig\":\"CAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg=\",\"key\":\"BwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBw==\"}";
+        let s = "{\"addr\":\"0x0707070707070707070707070707070707070707\",\"sig\":\"0x0808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808080808\",\"key\":\"0x07070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707070707\"}";
 
         // Print, write to a file, or send to an HTTP server.
         assert_eq!(s, j);
