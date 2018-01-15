e84ccdfd941e962527a9aba2d18a80781bf50510
kingoflolz <wangben3@gmail.com>
2018-01-15 20:38:18 +1100

    Get Identity over link local IP address

--- rita/tunnel_manager/src/lib.rs
+++ rita/tunnel_manager/src/lib.rs
@@ -4,7 +4,12 @@ extern crate derive_error;
 #[macro_use]
 extern crate log;
 
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv6Addr, SocketAddr, SocketAddrV6, SocketAddrV4, TcpStream};
+use std::time::Duration;
+
+use std::io::{Read, Write};
+
+extern crate serde_json;
 
 extern crate althea_types;
 use althea_types::EthAddress;
@@ -22,6 +27,8 @@ use reqwest::Client;
 pub enum Error {
     KernelInterfaceError(althea_kernel_interface::Error),
     HttpReqError(reqwest::Error),
+    IOError(std::io::Error),
+    DeserializationError(serde_json::Error),
     #[error(msg_embedded, no_from, non_std)] TunnelManagerError(String),
 }
 
@@ -57,7 +64,29 @@ impl TunnelManager {
     pub fn neighbor_inquiry(&mut self, ip: IpAddr, dev: &str) -> Result<Identity, Error> {
         let url = format!("http://[{}%25{}]:4876/hello", ip, dev);
         trace!("Saying hello to: {:?}", url);
-        Ok(self.client.get(&url).send()?.json()?)
+
+        let socket = match ip {
+            IpAddr::V6(ip_v6) => {
+                SocketAddr::V6(SocketAddrV6::new(ip_v6, 4876, 0, self.ki.get_iface_index(dev)?))
+            }
+            IpAddr::V4(ip_v4) => {
+                SocketAddr::V4(SocketAddrV4::new(ip_v4, 4876)) //TODO: Do we want to allow IPv4?
+            }
+        };
+
+        let mut stream = TcpStream::connect(socket)?;
+
+        // Format HTTP request
+        let mut header = format!("GET /hello HTTP/1.0\r\nHost: [{}%25{}]\r\n\r\n", ip, dev);  //TODO: check if this is a proper HTTP request
+        stream.write(header.as_bytes());
+
+        // Make request and return response as string
+        let mut resp = String::new();
+        stream.read_to_string(&mut resp)?;
+
+        trace!("They replied {}", resp);
+
+        Ok(serde_json::from_str(&resp)?)
     }
 }
 
