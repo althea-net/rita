22be42d3602bf8f6e34bbb2ef2a4394c03a43b19
kingoflolz <wangben3@gmail.com>
2018-01-16 23:22:38 +1100

    Started on bounty hunter integration

---
+++ rita/src/network_endpoints.rs
@@ -0,0 +1,34 @@
+use debt_keeper::{DebtKeeper, DebtAction, DebtAdjustment, Identity};
+
+use payment_controller::{PaymentTx, PaymentController};
+extern crate num256;
+use num256::Int256;
+
+use rouille::{Request, Response};
+
+extern crate serde;
+extern crate serde_json;
+
+extern crate rand;
+
+use std::sync::mpsc::Sender;
+use std::sync::{Mutex, Arc};
+use std::io::Read;
+
+pub fn make_payments(request: &Request, m_tx: Arc<Mutex<Sender<DebtAdjustment>>>) -> Response {
+    if let Some(mut data) = request.data() {
+        let mut pmt_str = String::new();
+        data.read_to_string(&mut pmt_str).unwrap();
+        let pmt: PaymentTx = serde_json::from_str(&pmt_str).unwrap();
+        m_tx.lock().unwrap().send(
+            DebtAdjustment {
+                ident: pmt.from,
+                amount: Int256::from(pmt.amount)
+            }
+        ).unwrap();
+        payment_received
+        Response::text("Payment Recieved")
+    } else {
+        Response::text("Payment Error")
+    }
+}
