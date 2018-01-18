87d753a9e80b46740a39505bc759adebeca4c3ac
Jehan <jehan.tremback@gmail.com>
2018-01-17 23:17:15 -0800

    separate out kernel_interface stuff into separate files

--- bounty_hunter/src/main.rs
+++ bounty_hunter/src/main.rs
@@ -1,22 +1,22 @@
-#[macro_use] extern crate log;
+// #[macro_use] extern crate log;
 
-#[macro_use] extern crate rouille;
+// #[macro_use] extern crate rouille;
+fn main() {}
+// fn main() {
+//     simple_logger::init().unwrap();
+//     trace!("Starting");
 
-fn main() {
-    simple_logger::init().unwrap();
-    trace!("Starting");
+//     rouille::start_server("[::0]:8080", move |request| { // TODO: fix the port
+//         router!(request,
+//             (POST) (/update) => {
+//                 // process_updates
+//                 rouille::Response::text(format!("hello, {}", id))
+//             },
+//             _ => rouille::Response::empty_404()
+//         )
+//     });
+// }
 
-    rouille::start_server("[::0]:8080", move |request| { // TODO: fix the port
-        router!(request,
-            (POST) (/update) => {
-                process_updates
-                rouille::Response::text(format!("hello, {}", id))
-            },
-            _ => rouille::Response::empty_404()
-        )
-    });
-}
+// fn process_updates() {
 
-fn process_updates() {
-
-}
+// }
