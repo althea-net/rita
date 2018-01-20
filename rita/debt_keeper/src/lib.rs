f232a9ecdcdbfd31ac334f753bfe65765a2b0028
kindiana <wangben3@gmail.com>
2018-01-20 22:32:53 +1100

    Add OpenTunnel to debt keeper, fix bugs

--- rita/debt_keeper/src/lib.rs
+++ rita/debt_keeper/src/lib.rs
@@ -6,7 +6,7 @@ extern crate derive_error;
 
 use std::net::IpAddr;
 use std::collections::HashMap;
-use std::ops::Add;
+use std::ops::{Add, Sub};
 
 extern crate serde;
 
@@ -14,13 +14,12 @@ extern crate althea_types;
 use althea_types::{EthAddress, Identity};
 
 extern crate num256;
-use num256::Uint256;
+use num256::{Uint256, Int256};
 
 extern crate eui48;
 use eui48::MacAddress;
 
 extern crate stash;
-use num256::Int256;
 
 #[derive(Debug, Error)]
 pub enum Error {
@@ -36,6 +35,7 @@ pub struct DebtKeeper {
 #[derive(Debug, PartialEq)]
 pub enum DebtAction {
     SuspendTunnel,
+    OpenTunnel,
     MakePayment(Uint256),
 }
 
@@ -47,6 +47,8 @@ pub struct DebtAdjustment {
 
 impl DebtKeeper {
     pub fn new(pay_threshold: Int256, close_threshold: Int256) -> Self {
+        assert!(pay_threshold > Int256::from(0));
+        assert!(close_threshold < Int256::from(0));
         DebtKeeper {
             debts: HashMap::new(),
             pay_threshold,
@@ -56,12 +58,16 @@ impl DebtKeeper {
 
     pub fn apply_debt(&mut self, ident: Identity, debt: Int256) -> Option<DebtAction> {
         let stored_debt = self.debts.entry(ident).or_insert(Int256::from(0));
+        let old_debt = stored_debt.clone();
+
         *stored_debt = stored_debt.clone().add(debt.clone());
 
-        if debt < self.close_threshold {
+        if *stored_debt < self.close_threshold {
             Some(DebtAction::SuspendTunnel)
-        } else if debt > self.pay_threshold {
-            Some(DebtAction::MakePayment(Uint256::from(debt)))
+        } else if (self.close_threshold < *stored_debt) && (old_debt < self.close_threshold) {
+            Some(DebtAction::OpenTunnel)
+        } else if *stored_debt > self.pay_threshold {
+            Some(DebtAction::MakePayment(Uint256::from(stored_debt.clone())))
         } else {
             None
         }
@@ -70,9 +76,11 @@ impl DebtKeeper {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(10));
+    fn test_single_suspend() {
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
 
         let ident = Identity {
             eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
@@ -83,8 +91,147 @@ mod tests {
         };
 
         assert_eq!(
-            d.apply_debt(ident, Int256::from(7)).unwrap(),
+            d.apply_debt(ident, Int256::from(-100)).unwrap(),
             DebtAction::SuspendTunnel
+        );
+    }
+
+    #[test]
+    fn test_single_pay() {
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+
+        let ident = Identity {
+            eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
+                .parse()
+                .unwrap(),
+            ip_address: "2001::3".parse().unwrap(),
+            mac_address: MacAddress::parse_str("00:00:00:aa:00:02").unwrap(),
+        };
+
+        assert_eq!(
+            d.apply_debt(ident, Int256::from(100)).unwrap(),
+            DebtAction::MakePayment(Uint256::from(100u32))
+        );
+    }
+
+    #[test]
+    fn test_single_reopen() {
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+
+        let ident = Identity {
+            eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
+                .parse()
+                .unwrap(),
+            ip_address: "2001::3".parse().unwrap(),
+            mac_address: MacAddress::parse_str("00:00:00:aa:00:02").unwrap(),
+        };
+
+        assert_eq!(
+            d.apply_debt(ident, Int256::from(-100)).unwrap(),
+            DebtAction::SuspendTunnel
+        );
+
+        assert_eq!(
+            d.apply_debt(ident, Int256::from(110)).unwrap(),
+            DebtAction::OpenTunnel
+        );
+    }
+
+    #[test]
+    fn test_multi_pay() {
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+
+        let ident = Identity {
+            eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
+                .parse()
+                .unwrap(),
+            ip_address: "2001::3".parse().unwrap(),
+            mac_address: MacAddress::parse_str("00:00:00:aa:00:02").unwrap(),
+        };
+
+        // send lots of payments
+        for i in 0..100 {
+            assert_eq!(
+                d.apply_debt(ident, Int256::from(100)).unwrap(),
+                DebtAction::MakePayment(Uint256::from(100u32 * (i + 1)))
+            );
+        }
+    }
+
+    #[test]
+    fn test_multi_fail() {
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+
+        let ident = Identity {
+            eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
+                .parse()
+                .unwrap(),
+            ip_address: "2001::3".parse().unwrap(),
+            mac_address: MacAddress::parse_str("00:00:00:aa:00:02").unwrap(),
+        };
+
+        // send lots of payments
+        for i in 0..100 {
+            assert_eq!(
+                d.apply_debt(ident, Int256::from(100)).unwrap(),
+                DebtAction::MakePayment(Uint256::from(100u32 * (i + 1)))
+            );
+        }
+
+        // rack up lots of debt
+        for i in 0..99 {
+            assert_eq!(
+                d.apply_debt(ident, Int256::from(-100)).unwrap(),
+                DebtAction::MakePayment(Uint256::from(100u32 * (99 - i)))
+            );
+        }
+
+        // should have +100 credit
+        assert_eq!(
+            d.apply_debt(ident, Int256::from(-200)).unwrap(),
+            DebtAction::SuspendTunnel
+        );
+    }
+
+    #[test]
+    fn test_multi_reopen() {
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+
+        let ident = Identity {
+            eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
+                .parse()
+                .unwrap(),
+            ip_address: "2001::3".parse().unwrap(),
+            mac_address: MacAddress::parse_str("00:00:00:aa:00:02").unwrap(),
+        };
+
+        // send lots of payments
+        for i in 0..100 {
+            assert_eq!(
+                d.apply_debt(ident, Int256::from(100)).unwrap(),
+                DebtAction::MakePayment(Uint256::from(100u32 * (i + 1)))
+            );
+        }
+
+        // rack up lots of debt
+        for i in 0..99 {
+            assert_eq!(
+                d.apply_debt(ident, Int256::from(-100)).unwrap(),
+                DebtAction::MakePayment(Uint256::from(100u32 * (99 - i)))
+            );
+        }
+
+        // should have +100 credit
+        assert_eq!(
+            d.apply_debt(ident, Int256::from(-200)).unwrap(),
+            DebtAction::SuspendTunnel
+        );
+
+        // should have -100 credit
+
+        assert_eq!(
+            d.apply_debt(ident, Int256::from(200)).unwrap(),
+            DebtAction::OpenTunnel
         );
     }
 }
