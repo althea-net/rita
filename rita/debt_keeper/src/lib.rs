3bbfdbeabdad0f53bf3caf4046f498f4bbef6282
kingoflolz <wangben3@gmail.com>
2018-02-01 21:53:04 +1100

    Refactor debtkeeper to enable easier addition of per node data

--- rita/debt_keeper/src/lib.rs
+++ rita/debt_keeper/src/lib.rs
@@ -33,11 +33,27 @@ pub enum Error {
     #[error(msg_embedded, no_from, non_std)] DebtKeeperError(String),
 }
 
+struct NodeDebtData {
+    incoming_payment: Uint256,
+    total_payment: Uint256,
+    debt: Int256,
+}
+
+impl NodeDebtData {
+    fn new() -> NodeDebtData {
+        NodeDebtData{
+            incoming_payment: Uint256::from(0u32),
+            total_payment: Uint256::from(0u32),
+            debt: Int256::from(0),
+        }
+    }
+}
+
 pub struct DebtKeeper {
-    incoming_payments: HashMap<Identity, Uint256>,
-    debts: HashMap<Identity, Int256>,
+    debt_data: HashMap<Identity, NodeDebtData>,
     pay_threshold: Int256,
-    close_threshold: Int256,
+    close_fraction: Int256,
+    close_threshold: Int256, // Connection is closed when debt < close_fraction * total_payment + close_threshold
 }
 
 /// The actions that a `DebtKeeper` can take.
@@ -59,10 +75,10 @@ pub enum DebtAction {
 
 
 impl DebtKeeper {
-    pub fn start(pay_threshold: Int256, close_threshold: Int256) ->
+    pub fn start(pay_threshold: Int256, close_threshold: Int256, close_fraction: Int256) ->
         (Sender<DebtKeeperMsg>, Receiver<Option<DebtAction>>)
     {
-        let mut keeper = DebtKeeper::new(pay_threshold, close_threshold);
+        let mut keeper = DebtKeeper::new(pay_threshold, close_threshold, close_fraction);
         let (input_tx, input_rx) = channel();
         let (output_tx, output_rx) = channel();
 
@@ -80,19 +96,19 @@ impl DebtKeeper {
         (input_tx, output_rx)
     }
 
-    pub fn new(pay_threshold: Int256, close_threshold: Int256) -> Self {
+    pub fn new(pay_threshold: Int256, close_threshold: Int256, close_fraction: Int256) -> Self {
         assert!(pay_threshold > Int256::from(0));
-        assert!(close_threshold < Int256::from(0));
+        assert!(close_fraction > Int256::from(0));
         DebtKeeper {
-            incoming_payments: HashMap::new(),
-            debts: HashMap::new(),
+            debt_data: HashMap::new(),
             pay_threshold,
-            close_threshold,
+            close_fraction: close_fraction,
+            close_threshold: close_threshold,
         }
     }
 
     fn apply_payment(&mut self, ident: Identity, amount: Uint256) {
-        let stored_balance = self.incoming_payments.entry(ident).or_insert(Uint256::from(0 as u32));
+        let stored_balance = &mut self.debt_data.entry(ident).or_insert(NodeDebtData::new()).incoming_payment;
         let old_balance = stored_balance.clone();
 
         trace!("apply_payment: old balance for {:?}: {:?}", ident.ip_address, old_balance);
@@ -104,11 +120,10 @@ impl DebtKeeper {
 
     /// This updates a neighbor's debt and outputs a DebtAction if one is necessary.
     fn apply_traffic(&mut self, ident: Identity, traffic: Int256) -> Option<DebtAction> {
-        let debt_ref = self.debts.entry(ident).or_insert(Int256::from(0));
-        let debt = debt_ref.clone();
+        let debt_data = self.debt_data.entry(ident).or_insert(NodeDebtData::new());
+        let debt = debt_data.debt.clone();
 
-        let payment_balance_ref = self.incoming_payments.entry(ident).or_insert(Uint256::from(0 as u32));
-        let payment_balance = payment_balance_ref.clone();
+        let payment_balance = debt_data.incoming_payment.clone();
 
         trace!(
             "apply_traffic for {:?}: debt: {:?}, traffic: {:?}, payment balance: {:?}",
@@ -118,21 +133,21 @@ impl DebtKeeper {
             payment_balance
         );
 
-        *payment_balance_ref = Uint256::from(0 as u32);
-        *debt_ref = debt.clone().add(traffic).add(Int256::from(payment_balance));
+        debt_data.incoming_payment = Uint256::from(0 as u32);
+        debt_data.debt = debt.clone().add(traffic).add(Int256::from(payment_balance));
 
-        trace!("new debt for {:?}: {:?}", ident.ip_address, *debt_ref);
+        trace!("new debt for {:?}: {:?}", ident.ip_address, debt_data.debt);
 
-        if *debt_ref < self.close_threshold {
+        if debt_data.debt < self.close_threshold {
             trace!("debt is below close threshold. suspending forwarding");
             Some(DebtAction::SuspendTunnel)
-        } else if (self.close_threshold < *debt_ref) && (debt < self.close_threshold) {
+        } else if (self.close_threshold < debt_data.debt) && (debt < self.close_threshold) {
             trace!("debt is above close threshold. resuming forwarding");
             Some(DebtAction::OpenTunnel)
-        } else if *debt_ref > self.pay_threshold {
+        } else if debt_data.debt > self.pay_threshold {
             trace!("debt is above payment threshold. making payment");
-            let d = debt_ref.clone();
-            *debt_ref = Int256::from(0);
+            let d = debt_data.debt.clone();
+            debt_data.debt = Int256::from(0);
             Some(DebtAction::MakePayment{to: ident, amount: Uint256::from(d)})
         } else {
             None
@@ -146,7 +161,7 @@ mod tests {
 
     #[test]
     fn test_single_suspend() {
-        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10), Int256::from(100));
 
         let ident = Identity {
             eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
@@ -164,7 +179,7 @@ mod tests {
 
     #[test]
     fn test_single_pay() {
-        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10), Int256::from(100));
 
         let ident = Identity {
             eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
@@ -182,7 +197,7 @@ mod tests {
 
     #[test]
     fn test_single_reopen() {
-        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10), Int256::from(100));
 
         let ident = Identity {
             eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
@@ -205,7 +220,7 @@ mod tests {
 
     #[test]
     fn test_multi_pay() {
-        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10), Int256::from(100));
 
         let ident = Identity {
             eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
@@ -228,7 +243,7 @@ mod tests {
 
     #[test]
     fn test_multi_fail() {
-        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10), Int256::from(100));
 
         let ident = Identity {
             eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
@@ -251,7 +266,7 @@ mod tests {
 
     #[test]
     fn test_multi_reopen() {
-        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10));
+        let mut d = DebtKeeper::new(Int256::from(5), Int256::from(-10), Int256::from(100));
 
         let ident = Identity {
             eth_address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe"
