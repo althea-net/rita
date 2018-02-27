36d01994e62def78b8a7b11c668abc51e32186d5
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-02-27 13:34:58 -0500

    WIP config bootstrapping
    This contains the new binary Clu which needs to be moved over to a
    library as we do the monorepo refacotr. This also adds post_office
    very very WIP right now it's a simple request server for the exit
    that takes new exit client requests and adds them to the exit database.

--- althea_kernel_interface/src/manipulate_uci.rs
+++ althea_kernel_interface/src/manipulate_uci.rs
@@ -1,11 +1,15 @@
-use super::{KernelInterface, Error};
+use super::{KernelInterface, KernelManagerError};
+
+use failure::Error;
 
 impl KernelInterface {
     //Sets an arbitrary UCI variable on OpenWRT
     pub fn set_uci_var(&mut self, key: &str, value: &str) -> Result<bool,Error> {
         let output = self.run_command("uci", &["set", &key, "=", &value])?;
         if !output.stderr.is_empty() {
-            return Err(Error::RuntimeError(format!("recieved error while setting UCI: {}", String::from_utf8(output.stderr)?)))
+            return Err(KernelManagerError::RuntimeError(format!(
+                "recieved error while setting UCI: {}",
+                String::from_utf8(output.stderr)?)).into())
         }
         Ok(true)
     }
@@ -14,7 +18,9 @@ impl KernelInterface {
     pub fn get_uci_var(&mut self, key: &str) -> Result<String,Error> {
         let output = self.run_command("uci", &["show", &key])?;
         if !output.stderr.is_empty() {
-            return Err(Error::RuntimeError(format!("recieved error while getting UCI: {}", String::from_utf8(output.stderr)?)))
+            return Err(KernelManagerError::RuntimeError(format!(
+                "recieved error while getting UCI: {}",
+                String::from_utf8(output.stderr)?)).into())
         }
         Ok(String::from_utf8(output.stdout)?)
     }
@@ -23,8 +29,10 @@ impl KernelInterface {
     pub fn uci_commit(&mut self) -> Result<bool,Error> {
         let output = self.run_command("uci", &["commit"])?;
         if !output.stderr.is_empty() {
-            return Err(Error::RuntimeError(format!("recieved error while commiting UCI: {}", String::from_utf8(output.stderr)?)))
-        }
+            return Err(KernelManagerError::RuntimeError(format!(
+                "recieved error while commiting UCI: {}",
+                String::from_utf8(output.stderr)?)).into())
+       }
         Ok(true)
     }
 }
