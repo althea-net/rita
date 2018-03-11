9bd7d0e8ebe93b59d3bc06bdd05e1241c28870bf
ijanus <emekamicro@gmail.com>
2018-03-11 04:41:06 +0100

    introduced macro invocation Removed redundant and repeated codes

--- althea_types/src/eth_address.rs
+++ althea_types/src/eth_address.rs
@@ -6,63 +6,6 @@ use serde;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::hash::{Hash, Hasher};
 
-#[derive(Copy, Clone)]
-pub struct EthAddress(pub [u8; 20]);
+use eth_macro;
 
-impl Hash for EthAddress {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.0.hash(state);
-    }
-}
-
-impl PartialEq for EthAddress {
-    fn eq(&self, other: &EthAddress) -> bool {
-        self.0[..] == other.0[..]
-    }
-}
-impl Eq for EthAddress {}
-
-impl Deref for EthAddress {
-    type Target = [u8; 20 as usize];
-
-    fn deref(&self) -> &[u8; 20 as usize] {
-        &self.0
-    }
-}
-
-impl FromStr for EthAddress {
-    type Err = hex::FromHexError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        hex::decode(&s[2..]).map(|v| {
-            let mut arr = [0u8; 20];
-            arr.clone_from_slice(&v);
-            EthAddress(arr)
-        })
-    }
-}
-
-impl fmt::Debug for EthAddress {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "EthAddress {}", self.to_string())
-    }
-}
-
-impl fmt::Display for EthAddress {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "0x{}", hex::encode(self.0.as_ref()))
-    }
-}
-
-impl Serialize for EthAddress {
-    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_str(&self.to_string())
-    }
-}
-
-impl<'de> Deserialize<'de> for EthAddress {
-    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let s = String::deserialize(deserializer)?;
-        s.parse().map_err(serde::de::Error::custom)
-    }
-}
+impl_eth!(EthAddress[u8; 20]);
