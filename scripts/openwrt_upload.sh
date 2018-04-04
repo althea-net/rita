2df1f110485b024ae6ef7a3909c68ad7bc285860
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-04-04 15:42:52 -0400

    Copy over test config file to routers

--- scripts/openwrt_upload.sh
+++ scripts/openwrt_upload.sh
@@ -3,4 +3,5 @@ set -eux
 bash scripts/openwrt_build.sh
 scp target/mipsel-unknown-linux-musl/debug/rita root@192.168.1.1:/tmp/rita
 scp settings/default.toml root@192.168.1.1:/etc/rita-default.toml
-ssh root@192.168.1.1 RUST_BACKTRACE=FULL RUST_LOG=INFO /tmp/rita --config /etc/rita-default.toml  --default /etc/rita-default.toml --platform openwrt
+scp scripts/rita-test.toml root@192.168.1.1:/etc/rita.toml
+ssh root@192.168.1.1 RUST_BACKTRACE=FULL RUST_LOG=TRACE /tmp/rita --config /etc/rita.toml  --default /etc/rita-default.toml --platform openwrt
