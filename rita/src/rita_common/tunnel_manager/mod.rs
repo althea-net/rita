211cc3c88055fdc8d14aa87005423c1e7572da45
kindiana <wangben3@gmail.com>
2018-04-16 19:17:00 +0800

    make manually configuring link local addresses optional

--- rita/src/rita_common/tunnel_manager/mod.rs
+++ rita/src/rita_common/tunnel_manager/mod.rs
@@ -345,6 +345,7 @@ impl TunnelManager {
             Path::new(&SETTING.get_network().wg_private_key_path),
             &SETTING.get_network().own_ip,
             SETTING.get_network().external_nic.clone(),
+            SETTING.get_network().conf_link_local
         )?;
 
         let mut babel = Babel::new(&format!("[::1]:{}", SETTING.get_network().babel_port)
