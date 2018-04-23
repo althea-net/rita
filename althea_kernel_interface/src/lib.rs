kingoflolz/althea_rs

updated testing infrastructure for kernel interface which allows for mocking of kernel interface from external crates and actor mocking

althea_kernel_interface/src/lib.rs
@@ -16,6 +16,7 @@ use std::io::Write;
 use std::process::{Command, Output};
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
+use std::path::Path;
 
 use std::str;
 
@@ -50,29 +51,27 @@ pub enum KernelInterfaceError {
 }
 
 #[cfg(test)]
-pub struct KernelInterface {
-    run_command: Arc<Mutex<Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>>>,
-}
-
-#[cfg(not(test))]
 lazy_static! {
-    pub static ref KI: KernelInterface = KernelInterface {};
-}
-
-#[cfg(test)]
-lazy_static! {
-    pub static ref KI: KernelInterface = KernelInterface {
+    pub static ref KI: Box<KernelInterface> = Box::new(TestCommandRunner {
         run_command: Arc::new(Mutex::new(Box::new(|program, args| {
             panic!("kernel interface used before initialized");
         })))
-    };
+    });
 }
 
 #[cfg(not(test))]
-pub struct KernelInterface {}
+lazy_static! {
+    pub static ref KI: Box<KernelInterface> = Box::new(LinuxCommandRunner {});
+}
 
-impl KernelInterface {
-    #[cfg(not(test))]
+pub trait CommandRunner {
+    fn run_command(&self, program: &str, args: &[&str]) -> Result<Output, Error>;
+    fn set_mock(&self, mock: Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>);
+}
+
+pub struct LinuxCommandRunner;
+
+impl CommandRunner for LinuxCommandRunner {
     fn run_command(&self, program: &str, args: &[&str]) -> Result<Output, Error> {
         let start = Instant::now();
         let output = Command::new(program).args(args).output()?;
@@ -92,12 +91,16 @@ impl KernelInterface {
         return Ok(output);
     }
 
-    #[cfg(test)]
     fn set_mock(&self, mock: Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>) {
-        *self.run_command.lock().unwrap() = mock
+        unimplemented!()
     }
+}
 
-    #[cfg(test)]
+pub struct TestCommandRunner {
+    pub run_command: Arc<Mutex<Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>>>,
+}
+
+impl CommandRunner for TestCommandRunner {
     fn run_command(&self, program: &str, args: &[&str]) -> Result<Output, Error> {
         let mut args_owned = Vec::new();
         for a in args {
@@ -106,4 +109,13 @@ impl KernelInterface {
 
         (&mut *self.run_command.lock().unwrap())(program.to_string(), args_owned)
     }
+
+    fn set_mock(&self, mock: Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>) {
+        *self.run_command.lock().unwrap() = mock
+    }
 }
+
+pub trait KernelInterface: CommandRunner + Sync{}
+
+impl KernelInterface for LinuxCommandRunner{}
+impl KernelInterface for TestCommandRunner{}

settings/src/lib.rs
@@ -36,7 +36,7 @@ use althea_types::{EthAddress, ExitRegistrationDetails, Identity};
 
 use num256::Int256;
 
-use althea_kernel_interface::KernelInterface;
+use althea_kernel_interface::{KI, KernelInterface};
 
 use failure::Error;
 
@@ -210,12 +210,10 @@ impl RitaCommonSettings<RitaSettingsStruct> for Arc<RwLock<RitaSettingsStruct>> 
     }
 
     fn get_identity(&self) -> Identity {
-        let ki = KernelInterface {};
         Identity::new(
             self.get_network().own_ip.clone(),
             self.get_payment().eth_address.clone(),
-            ki.get_wg_pubkey(Path::new(&self.get_network().wg_private_key_path))
-                .unwrap(),
+            self.get_network().wg_public_key.clone()
         )
     }
 }
@@ -246,12 +244,10 @@ impl RitaCommonSettings<RitaExitSettingsStruct> for Arc<RwLock<RitaExitSettingsS
     }
 
     fn get_identity(&self) -> Identity {
-        let ki = KernelInterface {};
         Identity::new(
             self.get_network().own_ip.clone(),
             self.get_payment().eth_address.clone(),
-            ki.get_wg_pubkey(Path::new(&self.get_network().wg_private_key_path))
-                .unwrap(),
+            self.get_network().wg_public_key.clone()
         )
     }
 }
@@ -380,23 +376,15 @@ where
     let file_path = file_path.to_string();
 
     thread::spawn(move || {
-        info!("Watching file {} for activity...", file_path);
-
         let old_settings = settings.read().unwrap().clone();
 
         loop {
-            info!("refreshing configuration ...");
             thread::sleep(Duration::from_secs(5));
 
             let new_settings = settings.read().unwrap().clone();
 
-            if old_settings == new_settings {
-                // settings struct was not mutated locally
-                let config = config.refresh().unwrap();
-                let new_settings: T = config.clone().try_into().unwrap();
-                trace!("new config: {:#?}", new_settings);
-                *settings.write().unwrap() = new_settings;
-            } else {
+            if old_settings != new_settings {
+                info!("writing updated config: {:?}", new_settings);
                 settings.read().unwrap().write(&file_path);
             }
         }
@@ -452,10 +440,6 @@ impl RitaExitSettingsStruct {
         let mut s = Config::new();
         s.merge(config::File::with_name(file_name).required(false))?;
         let settings: Self = s.try_into()?;
-
-        let mut file = File::create(&Path::new(&settings.network.wg_private_key_path))?;
-        file.write_all(&settings.network.wg_private_key.as_bytes())?;
-
         Ok(settings)
     }
 
@@ -463,9 +447,6 @@ impl RitaExitSettingsStruct {
         let mut s = Config::new();
         s.merge(config::File::with_name(file_name).required(false))?;
         let settings: Self = s.clone().try_into()?;
-
-        let mut file = File::create(&Path::new(&settings.network.wg_private_key_path))?;
-        file.write_all(&settings.network.wg_private_key.as_bytes())?;
 
         let settings = Arc::new(RwLock::new(settings));
 

althea-net/rita

althea_kernel_interface/src/lib.rs
@@ -13,6 +13,7 @@ use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::ffi::OsStr;
 use std::io::Write;
+use std::path::Path;
 use std::process::{Command, Output};
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
@@ -50,29 +51,27 @@ pub enum KernelInterfaceError {
 }
 
 #[cfg(test)]
-pub struct KernelInterface {
-    run_command: Arc<Mutex<Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>>>,
-}
-
-#[cfg(not(test))]
 lazy_static! {
-    pub static ref KI: KernelInterface = KernelInterface {};
-}
-
-#[cfg(test)]
-lazy_static! {
-    pub static ref KI: KernelInterface = KernelInterface {
+    pub static ref KI: Box<KernelInterface> = Box::new(TestCommandRunner {
         run_command: Arc::new(Mutex::new(Box::new(|program, args| {
             panic!("kernel interface used before initialized");
         })))
-    };
+    });
 }
 
 #[cfg(not(test))]
-pub struct KernelInterface {}
+lazy_static! {
+    pub static ref KI: Box<KernelInterface> = Box::new(LinuxCommandRunner {});
+}
 
-impl KernelInterface {
-    #[cfg(not(test))]
+pub trait CommandRunner {
+    fn run_command(&self, program: &str, args: &[&str]) -> Result<Output, Error>;
+    fn set_mock(&self, mock: Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>);
+}
+
+pub struct LinuxCommandRunner;
+
+impl CommandRunner for LinuxCommandRunner {
     fn run_command(&self, program: &str, args: &[&str]) -> Result<Output, Error> {
         let start = Instant::now();
         let output = Command::new(program).args(args).output()?;
@@ -92,12 +91,16 @@ impl KernelInterface {
         return Ok(output);
     }
 
-    #[cfg(test)]
     fn set_mock(&self, mock: Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>) {
-        *self.run_command.lock().unwrap() = mock
+        unimplemented!()
     }
+}
 
-    #[cfg(test)]
+pub struct TestCommandRunner {
+    pub run_command: Arc<Mutex<Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>>>,
+}
+
+impl CommandRunner for TestCommandRunner {
     fn run_command(&self, program: &str, args: &[&str]) -> Result<Output, Error> {
         let mut args_owned = Vec::new();
         for a in args {
@@ -106,4 +109,13 @@ impl KernelInterface {
 
         (&mut *self.run_command.lock().unwrap())(program.to_string(), args_owned)
     }
+
+    fn set_mock(&self, mock: Box<FnMut(String, Vec<String>) -> Result<Output, Error> + Send>) {
+        *self.run_command.lock().unwrap() = mock
+    }
 }
+
+pub trait KernelInterface: CommandRunner + Sync {}
+
+impl KernelInterface for LinuxCommandRunner {}
+impl KernelInterface for TestCommandRunner {}

settings/src/lib.rs
@@ -36,7 +36,7 @@ use althea_types::{EthAddress, ExitRegistrationDetails, Identity};
 
 use num256::Int256;
 
-use althea_kernel_interface::KernelInterface;
+use althea_kernel_interface::{KernelInterface, KI};
 
 use failure::Error;
 
@@ -210,12 +210,10 @@ impl RitaCommonSettings<RitaSettingsStruct> for Arc<RwLock<RitaSettingsStruct>> 
     }
 
     fn get_identity(&self) -> Identity {
-        let ki = KernelInterface {};
         Identity::new(
             self.get_network().own_ip.clone(),
             self.get_payment().eth_address.clone(),
-            ki.get_wg_pubkey(Path::new(&self.get_network().wg_private_key_path))
-                .unwrap(),
+            self.get_network().wg_public_key.clone(),
         )
     }
 }
@@ -246,12 +244,10 @@ impl RitaCommonSettings<RitaExitSettingsStruct> for Arc<RwLock<RitaExitSettingsS
     }
 
     fn get_identity(&self) -> Identity {
-        let ki = KernelInterface {};
         Identity::new(
             self.get_network().own_ip.clone(),
             self.get_payment().eth_address.clone(),
-            ki.get_wg_pubkey(Path::new(&self.get_network().wg_private_key_path))
-                .unwrap(),
+            self.get_network().wg_public_key.clone(),
         )
     }
 }
@@ -380,23 +376,15 @@ where
     let file_path = file_path.to_string();
 
     thread::spawn(move || {
-        info!("Watching file {} for activity...", file_path);
-
         let old_settings = settings.read().unwrap().clone();
 
         loop {
-            info!("refreshing configuration ...");
             thread::sleep(Duration::from_secs(5));
 
             let new_settings = settings.read().unwrap().clone();
 
-            if old_settings == new_settings {
-                // settings struct was not mutated locally
-                let config = config.refresh().unwrap();
-                let new_settings: T = config.clone().try_into().unwrap();
-                trace!("new config: {:#?}", new_settings);
-                *settings.write().unwrap() = new_settings;
-            } else {
+            if old_settings != new_settings {
+                info!("writing updated config: {:?}", new_settings);
                 settings.read().unwrap().write(&file_path);
             }
         }
@@ -452,10 +440,6 @@ impl RitaExitSettingsStruct {
         let mut s = Config::new();
         s.merge(config::File::with_name(file_name).required(false))?;
         let settings: Self = s.try_into()?;
-
-        let mut file = File::create(&Path::new(&settings.network.wg_private_key_path))?;
-        file.write_all(&settings.network.wg_private_key.as_bytes())?;
-
         Ok(settings)
     }
 
@@ -463,9 +447,6 @@ impl RitaExitSettingsStruct {
         let mut s = Config::new();
         s.merge(config::File::with_name(file_name).required(false))?;
         let settings: Self = s.clone().try_into()?;
-
-        let mut file = File::create(&Path::new(&settings.network.wg_private_key_path))?;
-        file.write_all(&settings.network.wg_private_key.as_bytes())?;
 
         let settings = Arc::new(RwLock::new(settings));
 