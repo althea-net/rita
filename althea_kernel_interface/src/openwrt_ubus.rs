5d6a579fb03b5a3856c1f481ea26061c53fbbdce
kindiana <wangben3@gmail.com>
2018-04-28 17:24:09 +0800

    more dashboard bugfixes

--- althea_kernel_interface/src/openwrt_ubus.rs
+++ althea_kernel_interface/src/openwrt_ubus.rs
@@ -6,7 +6,12 @@ use failure::Error;
 
 impl KernelInterface {
     /// calls a ubus rpc
-    pub fn ubus_call(&self, namespace: &str, function: &str, argument: &str) -> Result<String, Error> {
+    pub fn ubus_call(
+        &self,
+        namespace: &str,
+        function: &str,
+        argument: &str,
+    ) -> Result<String, Error> {
         let output = String::from_utf8(
             self.run_command("ubus", &["call", namespace, function, argument])?
                 .stdout,
