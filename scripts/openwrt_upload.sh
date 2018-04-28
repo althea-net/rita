7c72f096ff4a6b12131ef9df81616ec95f445a86
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-04-28 17:31:29 -0400

    Update build scripts
    These where getting a little annoying, copying over the example
    config is no longer needed since we now ship with one in the firmware
    also we probably shouldn't commit our own build dir paths.

--- scripts/openwrt_upload.sh
+++ scripts/openwrt_upload.sh
@@ -2,6 +2,4 @@
 set -eux
 bash scripts/openwrt_build.sh
 scp target/mipsel-unknown-linux-musl/debug/rita root@192.168.1.1:/tmp/rita
-scp settings/default.toml root@192.168.1.1:/etc/rita-default.toml
-scp scripts/rita-test.toml root@192.168.1.1:/etc/rita.toml
-ssh root@192.168.1.1 RUST_BACKTRACE=FULL RUST_LOG=TRACE /tmp/rita --config /etc/rita.toml  --default /etc/rita-default.toml --platform openwrt
+ssh root@192.168.1.1 RUST_BACKTRACE=FULL RUST_LOG=TRACE /tmp/rita --config /etc/rita.toml --platform linux &> out.log
