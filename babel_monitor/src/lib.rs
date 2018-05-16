e0a6f56d43538a6d82b6a8b7d31d3cdef22d0c78
Stan Drozd <drozdziak1@gmail.com>
2018-05-16 17:15:22 +0200

    babel_monitor: Add a full_path_rtt field to Route

--- babel_monitor/src/lib.rs
+++ babel_monitor/src/lib.rs
@@ -61,6 +61,7 @@ pub struct Route {
     pub prefix: IpNetwork,
     pub metric: u16,
     pub refmetric: u16,
+    pub full_path_rtt: f32,
     pub price: u32,
     pub fee: u32,
 }
@@ -220,6 +221,7 @@ impl<T: Read + Write> Babel<T> {
                     prefix: find_babel_val("prefix", entry)?.parse()?,
                     metric: find_babel_val("metric", entry)?.parse()?,
                     refmetric: find_babel_val("refmetric", entry)?.parse()?,
+                    full_path_rtt: find_babel_val("full-path-rtt", entry)?.parse()?,
                     price: find_babel_val("price", entry)?.parse()?,
                     fee: find_babel_val("fee", entry)?.parse()?,
                 });
