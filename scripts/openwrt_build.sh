df1b7fc28ed2eb48c2cf02887b774e5f650324f3
Jehan <jehan.tremback@gmail.com>
2018-05-20 17:18:41 -0700

    jehan trying to buidl

--- scripts/openwrt_build.sh
+++ scripts/openwrt_build.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
-export OPENWRT_DIR=CHANGEME
-export TOOLCHAIN=toolchain-mipsel_24kc_gcc-7.3.0_musl
-export TARGET_NAME=target-mipsel_24kc_musl
-export TARGET=mipsel-unknown-linux-musl
-export RUST_TRIPLE=$TARGET
-export CROSS_PREFIX=mipsel-openwrt-linux-musl
-export CROSS_COMPILE=$CROSS_PREFIX
-export TARGET_CC=$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/mipsel-openwrt-linux-musl-gcc
-export TARGET_CCX=$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/mipsel-openwrt-linux-musl-g++
-export TARGET_LD=$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/mipsel-openwrt-linux-musl-ld
-export TARGET_AR=$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/mipsel-openwrt-linux-musl-ar
-export PATH=$PATH:$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/
+export OPENWRT_DIR=$1
+export TOOLCHAIN=toolchain-mips_24kc_gcc-7.3.0_musl
+export TARGET_NAME=target-mips_24kc_musl
+export TARGET=mips-unknown-linux-musl
+export CROSS_PREFIX=mips-openwrt-linux-musl
 export HOST_CC=gcc
 export HOST=x86_64-unknown-linux-gnu
-export OPENSSL_DIR=$OPENWRT_DIR/staging_dir/$TARGET_NAME/usr/
-export LD_LIBRARY_PATH=/home/ben/src/althea-firmware/build/staging_dir/target-mipsel_24kc_musl/usr/lib/
-export LIBRARY_PATH=$OPENWRT_DIR/staging_dir/$TARGET_NAME/usr/lib/
-export SQLITE3_LIB_DIR=$LIBRARY_PATH
-export SQLITE3_LIB_DIR=$LIBRARY_PATH
 export PKG_CONFIG_ALLOW_CROSS=1
+
+export RUST_TRIPLE=$TARGET
+export CROSS_COMPILE=$CROSS_PREFIX
+export TARGET_CC=$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/mips-openwrt-linux-musl-gcc
+export TARGET_CCX=$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/mips-openwrt-linux-musl-g++
+export TARGET_LD=$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/mips-openwrt-linux-musl-ld
+export TARGET_AR=$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/mips-openwrt-linux-musl-ar
+export PATH=$PATH:$OPENWRT_DIR/staging_dir/$TOOLCHAIN/bin/
+export OPENSSL_DIR=$OPENWRT_DIR/staging_dir/$TARGET_NAME/usr/
+export LIBRARY_PATH=$OPENWRT_DIR/staging_dir/$TARGET_NAME/usr/lib/
+export LD_LIBRARY_PATH=$OPENWRT_DIR/staging_dir/$TARGET_NAME/usr/lib/
+export SQLITE3_LIB_DIR=$LIBRARY_PATH
+export SQLITE3_LIB_DIR=$LIBRARY_PATH
 export RUSTFLAGS="-C linker=$TARGET_CC -C ar=$TARGET_AR"
 cargo build --all --target $RUST_TRIPLE
