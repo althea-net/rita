8845c2b2940de00fd00edee11f0ee9c21ac63fcb
Stan Drozd <drozdziak1@gmail.com>
2018-06-08 17:07:00 +0200

    Add backwards-compat to integration tests
    This commit enables us and CI to perform our integration tests between
    two arbitrary althea_rs releases. By default it compares
    althea-mesh/althea_rs' master and release branches.
    
    Note: the tests might go nuts if the way we read data in the tests off
    Rita changes - i.e. a situation where two releases work perfectly in
    production and don't pass integration tests is possible.
    
    .gitignore:
    * Start ignoring temporary release repo directories
    
    .travis.yml:
    * Start testing against the random, old_exit and new_exit compat test
    layouts
    
    rita.py:
    * Add a mapping between nodes and assigned releases; use each release's
    binaries accordingly
    * Display release information in printouts
    * Make some of the global variables full caps
    
    rita.sh:
    * Remove redundant functions
    * Automatically decide whether to pull and build Babel
    * If COMPAT_LAYOUT is chosen pull and build the specified releases or
    use the defaults; if not, fall back to previous behavior

--- integration-tests/rita.sh
+++ integration-tests/rita.sh
@@ -1,58 +1,96 @@
 #!/usr/bin/env bash
+BABELD_DIR="deps/babeld"
+NETLAB_PATH="deps/network-lab/network-lab.sh"
+
+REMOTE_A=${REMOTE_A:=https://github.com/althea-mesh/althea_rs.git}
+REVISION_A=${REVISION_A:=master}
+DIR_A=${DIR_A:=althea_rs_a} # Don't override without good reason, this one and $DIR_B are git ignored
+
+REMOTE_B=${REMOTE_B:=$REMOTE_A}
+REVISION_B=${REVISION_B:=release}
+DIR_B=${DIR_B:=althea_rs_b}
+
 set -euxo pipefail
 
-cd $(dirname $0)
+cd $(dirname $0) # Make the script runnable from anywhere
 
-build_babel () {
-  rm -rf "deps/babeld"
-  git clone -b pre-0.1.0 "https://github.com/althea-mesh/babeld.git" "deps/babeld"
+build_rev() {
+  remote=$1
+  revision=$2
+  dir=$3
 
-  pushd deps/babeld
-  make
-  popd
-}
-
-get_python_deps () {
-  sudo pip3 install -r requirements.txt
-}
-
-fetch_netlab () {
-  if [ ! -d "deps/network-lab" ] ; then
-    git clone "https://github.com/kingoflolz/network-lab" "deps/network-lab" # TODO: Change this back when PR is upstreamed
+  if [ -z "${NO_PULL-}" ] ; then
+    rm -rf $dir
+    git clone $remote $dir
   fi
 
-  chmod +x deps/network-lab deps/network-lab/network-lab.sh
+  pushd $dir
+
+    git checkout $revision
+
+    cargo build --all
+
+    # Exit database
+    pushd exit_db
+      rm -rf test.db
+      diesel migration run
+    popd
+
+    # Bounty hunter database
+    pushd bounty_hunter
+      rm -rf test.db
+      diesel migration run
+    popd
+
+  popd
+
+  cp $dir/bounty_hunter/test.db .
 }
 
-build_rita () {
-  pushd ../rita
-  cargo build --all
-  popd
-  pushd ../exit_db
-  rm -rf test.db
-  diesel migration run
-  popd
-}
+sudo pip3 install -r requirements.txt
 
-build_bounty () {
-  pushd ../bounty_hunter
-  cargo build
-  rm -rf test.db
-  diesel migration run
-  rm -rf ../integration-tests/test.db
-  cp test.db ../integration-tests/test.db
-  popd
-}
-
-get_python_deps
-fetch_netlab
-
-if [ ! -z "${BUILD_BABELD-}" ]; then
-    build_babel
+if [ ! -f "${NETLAB_PATH-}" ] ; then
+  git clone "https://github.com/kingoflolz/network-lab" "deps/network-lab" # TODO: Change this back when PR is upstreamed
 fi
 
-build_rita
-build_bounty
+chmod +x deps/network-lab deps/network-lab/network-lab.sh
 
-pwd
-sudo -E python3 rita.py "$@"
+# Build Babel if not built
+if [ ! -f "${BABELD_DIR-}/babeld" ]; then
+  rm -rf $BABELD_DIR
+  git clone -b master https://github.com/althea-mesh/babeld.git $BABELD_DIR
+  make -C $BABELD_DIR
+fi
+
+
+# Only care about revisions if a compat layout was picked
+if [ ! -z "${COMPAT_LAYOUT-}" ] ; then
+  build_rev $REMOTE_A $REVISION_A $DIR_A
+  export RITA_A="$DIR_A/target/debug/rita"
+  export RITA_EXIT_A="$DIR_A/target/debug/rita_exit"
+  export BOUNTY_HUNTER_A="$DIR_A/target/debug/bounty_hunter"
+
+
+  build_rev $REMOTE_B $REVISION_B $DIR_B
+  export RITA_B="$DIR_B/target/debug/rita"
+  export RITA_EXIT_B="$DIR_B/target/debug/rita_exit"
+  export BOUNTY_HUNTER_B="$DIR_B/target/debug/bounty_hunter"
+else
+  pushd ..
+    cargo build --all
+
+    pushd exit_db
+      rm -rf test.db
+      diesel migration run
+    popd
+
+    pushd bounty_hunter
+      rm -rf test.db
+      diesel migration run
+    popd
+  popd
+
+  cp ../bounty_hunter/test.db .
+fi
+
+sudo -E python3 rita.py $@
