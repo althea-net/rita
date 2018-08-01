7b4a0f50c7dbf4d70d13a8db02cb6fd7ade3952c
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-08-01 17:36:57 -0400

    Add ipq40xx arm target
    Turns out the glb1300 is a little different than the Omnia, this accounts
    for that.

--- scripts/openwrt_build_ipq40xx.sh
+++ scripts/openwrt_build_ipq40xx.sh
@@ -5,14 +5,14 @@ if [[ ! -d $DIR/staging_dir ]]; then
     wget -N https://updates.altheamesh.com/staging.tar.xz -O staging.tar.xz > /dev/null; tar -xf staging.tar.xz
 fi
 
-export TOOLCHAIN=toolchain-arm_cortex-a9+vfpv3_gcc-7.3.0_musl_eabi
+export TOOLCHAIN=toolchain-arm_cortex-a7+neon-vfpv4_gcc-7.3.0_musl_eabi
 export TARGET_CC=$DIR/staging_dir/$TOOLCHAIN/bin/arm-openwrt-linux-gcc
 export TARGET_LD=$DIR/staging_dir/$TOOLCHAIN/bin/arm-openwrt-linux-ld
 export TARGET_AR=$DIR/staging_dir/$TOOLCHAIN/bin/arm-openwrt-linux-ar
 export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_LINKER=$TARGET_CC
 export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_AR=$TARGET_AR
-export SQLITE3_LIB_DIR=$DIR/staging_dir/target-arm_cortex-a9+vfpv3_musl_eabi/usr/lib/
-export ARMV7_UNKNOWN_LINUX_MUSLEABIHF_OPENSSL_DIR=$DIR/staging_dir/target-arm_cortex-a9+vfpv3_musl_eabi/usr/
+export SQLITE3_LIB_DIR=$DIR/staging_dir/target-arm_cortex-a7+neon-vfpv4_musl_eabi/usr/lib/
+export ARMV7_UNKNOWN_LINUX_MUSLEABIHF_OPENSSL_DIR=$DIR/staging_dir/target-arm_cortex-a7+neon-vfpv4_musl_eabi/usr/
 export OPENSSL_STATIC=1
 
 rustup target add armv7-unknown-linux-musleabihf
