62a46ffad6088d4563d4dbefda90154c07f9768a
Michał Papierski <michal@papierski.net>
2018-08-08 15:20:18 +0200

    Recrease WG keys with interface removal.

--- rita/src/rita_common/dashboard/network_endpoints.rs
+++ rita/src/rita_common/dashboard/network_endpoints.rs
@@ -1,5 +1,9 @@
 use actix::registry::SystemService;
-use actix_web::*;
+use clu::cleanup;
+use clu::linux_generate_mesh_ip;
+use clu::linux_generate_wg_keys;
+use std::path::Path;
+use KI;
 
 use futures::Future;
 
@@ -13,7 +17,8 @@ use settings::RitaCommonSettings;
 use SETTING;
 
 use super::{Dashboard, GetOwnInfo, OwnInfo};
-use actix_web::{error, fs, http, HttpResponse};
+use actix_web::*;
+
 use rita_common::network_endpoints::JsonStatusResponse;
 
 pub fn get_own_info(_req: HttpRequest) -> Box<Future<Item = Json<OwnInfo>, Error = Error>> {
@@ -39,11 +44,57 @@ pub fn set_settings(
     JsonStatusResponse::new(Ok("New settings applied".to_string()))
 }
 
-pub fn wipe(_req: HttpRequest) -> impl Responder {
+pub fn wipe(_req: HttpRequest) -> Result<HttpResponse, Error> {
     if cfg!(not(debug_assertions)) {
-        return HttpResponse::new(http::StatusCode::NOT_FOUND);
+        return Ok(HttpResponse::NotFound().finish());
     }
 
-    // TODO: Implement wiping logic
-    HttpResponse::new(http::StatusCode::NO_CONTENT)
+    // Clean up existing WG interfaces
+    match cleanup() {
+        Ok(_) => trace!("Cleanup success!"),
+        Err(e) => {
+            warn!("Unable to complete cleanup: {:?}", e);
+            return Err(e);
+        }
+    }
+
+    // Restore default route
+    match KI.restore_default_route(&mut SETTING.get_network_mut().default_route) {
+        Ok(_) => trace!("Restore default route success!"),
+        Err(e) => {
+            warn!("Unable to restore default route: {:?}", e);
+            return Err(e);
+        }
+    }
+
+    // Create new WireGuard keys
+    match linux_generate_wg_keys(&mut SETTING.get_network_mut()) {
+        Ok(_) => trace!("Generated new WireGuard keys"),
+        Err(e) => {
+            warn!("Unable to generate new WireGuard keys: {:?}", e);
+            return Err(e);
+        }
+    }
+    // Generate new mesh IP
+    match linux_generate_mesh_ip(&mut SETTING.get_network_mut()) {
+        Ok(_) => trace!("Generated new mesh IP"),
+        Err(e) => {
+            warn!("Unable to generate new mesh IP: {:?}", e);
+            return Err(e);
+        }
+    }
+
+    // Creates file on disk containing key
+    match KI.create_wg_key(
+        &Path::new(&SETTING.get_network().wg_private_key_path),
+        &SETTING.get_network().wg_private_key,
+    ) {
+        Ok(_) => trace!("Generated new WireGuard keys"),
+        Err(e) => {
+            warn!("Unable to generate new WireGuard keys: {:?}", e);
+            return Err(e);
+        }
+    }
+
+    Ok(HttpResponse::NoContent().finish())
 }
