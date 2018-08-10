5747f5ed8e96e5e172dde096b2bf94b53bb2c2ad
Michał Papierski <michal@papierski.net>
2018-08-10 18:04:54 +0200

    More info about `build_common.sh` logic.

--- scripts/build_common.sh
+++ scripts/build_common.sh
@@ -1,3 +1,25 @@
+# This bash script contains shared logic that is used to parse
+# command line arguments. It is meant to be sourced from within the
+# build scripts.
+# This script parses command line arguments and exposes variables such
+# as $PROFILE, and $FEATURES.
+#
+# Variables:
+#
+# $PROFILE: Can contain "--release" or "". By default it is set to
+#           "--release".
+# $FEATURES: Contains a value of a feature passed by "--feature foo",
+#            as "--feature foo". In case of no --feature switch is
+#            found in $@ then empty string is used.
+#
+# Example:
+#
+# $ source ./build_common.sh --release --feature foo
+# $ echo $FEATURES
+# --features foo
+# $ echo $PROFILE
+# --release
+
 # Builds release profile unless specified other
 PROFILE="--release"
 # Features switch that will be passed to cargo in a docker container
