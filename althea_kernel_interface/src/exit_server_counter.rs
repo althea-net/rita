0fb18eab0233288b061318c8396b65c58ff233ff
Michał Papierski <michal@papierski.net>
2018-08-13 17:12:25 +0200

    Implement unit tests for parsing ipset output.

--- althea_kernel_interface/src/exit_server_counter.rs
+++ althea_kernel_interface/src/exit_server_counter.rs
@@ -1,7 +1,7 @@
 use super::KernelInterface;
 
 use std::collections::HashMap;
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv6Addr};
 use std::str::FromStr;
 
 use regex::Regex;
@@ -56,6 +56,36 @@ fn parse_exit_ipset(input: &str) -> Result<HashMap<IpAddr, u64>, Error> {
         );
     }
     Ok(map)
+}
+
+#[test]
+fn test_parse_exit_ipset() {
+    let data = r#"
+add asdf fd00::1337:123 packets 123456789 bytes 987654321
+add zxcv fd00::b4dc:0d3 packets 123456789 bytes 987654320
+"#;
+    let result = parse_exit_ipset(data);
+    match result {
+        Ok(result) => {
+            assert_eq!(result.len(), 2);
+            println!("result {:?}", result);
+
+            let addr1 = Ipv6Addr::new(0xfd00, 0, 0, 0, 0, 0, 0x1337, 0x0123);
+            let value1 = result
+                .get(&IpAddr::V6(addr1))
+                .expect("Unable to find key 1");
+            assert_eq!(value1, &(987654321u64 + 123456789u64 * 80));
+
+            let addr2 = Ipv6Addr::new(0xfd00, 0, 0, 0, 0, 0, 0xb4dc, 0x00d3);
+            let value2 = result
+                .get(&(IpAddr::V6(addr2)))
+                .expect("Unable to find key 2");
+            assert_eq!(value2, &(987654320u64 + 123456789u64 * 80));
+        }
+        Err(e) => {
+            panic!("Unexpected error {:?}", e);
+        }
+    }
 }
 
 impl KernelInterface {
