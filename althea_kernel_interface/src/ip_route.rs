704b0d0259d7e0c8e56835a98c60257f733d3df2
Michał Papierski <michal@papierski.net>
2018-08-14 14:10:26 +0200

    Add tests for default route routine.

--- althea_kernel_interface/src/ip_route.rs
+++ althea_kernel_interface/src/ip_route.rs
@@ -102,3 +102,55 @@ impl KernelInterface {
         Ok(())
     }
 }
+
+#[test]
+fn test_get_default_route() {
+    use std::os::unix::process::ExitStatusExt;
+    use std::process::ExitStatus;
+    use std::process::Output;
+    use KI;
+    let mut counter = 0;
+
+    // This will mock `run_command` to run a real output of `ip route`
+    // with addition that there are additional spaces, more than one default
+    // route etc.
+    KI.set_mock(Box::new(move |program, args| {
+        counter += 1;
+        match counter {
+            1 => {
+                assert_eq!(program, "ip");
+                assert_eq!(args, vec!["route", "list", "default"]);
+                Ok(Output {
+                    stdout: b"
+169.254.0.0/16 dev wifiinterface scope link metric 1000
+172.16.82.0/24   dev vmnet1 proto kernel scope link src 172.16.82.1
+default   via   192.168.8.1   dev wifiinterface proto dhcp   metric 600
+172.17.0.0/16 dev docker0 proto kernel scope link src 172.17.0.1 linkdown
+192.168.8.0/24 dev wifiinterface proto kernel scope link src 192.168.8.175 metric 600
+default via 192.168.9.1 dev wifiinterface proto dhcp metric 1200
+192.168.36.0/24 dev vmnet8 proto kernel scope link src 192.168.36.1"
+                        .to_vec(),
+                    stderr: b"".to_vec(),
+                    status: ExitStatus::from_raw(0),
+                })
+            }
+            _ => panic!("Unexpected call {} {:?} {:?}", counter, program, args),
+        }
+    }));
+
+    let result = KI.get_default_route().expect("Unable to get default route");
+    assert_eq!(
+        result,
+        vec![
+            "default",
+            "via",
+            "192.168.8.1",
+            "dev",
+            "wifiinterface",
+            "proto",
+            "dhcp",
+            "metric",
+            "600"
+        ]
+    );
+}
