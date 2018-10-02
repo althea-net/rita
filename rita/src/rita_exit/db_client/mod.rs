5f9559beb24b2213aced58165887c9d1177c28ed
Stan Drozd <drozdziak1@gmail.com>
2018-10-02 10:54:07 +0200

    Add an enum for specifying the exit verification mode
    This commit adds an optional field to ExitDetails allowing the exits to
    share the verification strategy they use for registration. Currently
    always set to Off.

--- rita/src/rita_exit/db_client/mod.rs
+++ rita/src/rita_exit/db_client/mod.rs
@@ -42,7 +42,7 @@ use ipnetwork::IpNetwork;
 
 use failure::Error;
 
-use althea_types::{ExitClientDetails, ExitClientIdentity, ExitDetails, ExitState};
+use althea_types::{ExitClientDetails, ExitClientIdentity, ExitDetails, ExitState, ExitVerifMode};
 
 #[derive(Default)]
 pub struct DbClient;
@@ -144,6 +144,7 @@ pub fn get_exit_info() -> ExitDetails {
         exit_price: SETTING.get_exit_network().exit_price,
         netmask: SETTING.get_exit_network().netmask,
         description: SETTING.get_description(),
+        verif_mode: Some(ExitVerifMode::Off),
     }
 }
 
