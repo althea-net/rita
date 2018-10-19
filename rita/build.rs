3fc1ca4f1c8c4044e6cb5e14f63fca931b453ef0
Michał Papierski <michal@papierski.net>
2018-10-19 11:15:31 +0200

    Add a default GIT_HASH as fallback.
    This will present something like "(unknown)" which will be triggered
    only if build without git repository present. This will *only* happen
    with cross[1] which doesn't bundle git executable within docker images.
    
    [1]: https://github.com/japaric/rust-cross

--- rita/build.rs
+++ rita/build.rs
@@ -1,9 +1,11 @@
 use std::process::Command;
+
 fn main() {
-    let output = Command::new("git")
+    let git_hash = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+        .ok()
+        .and_then(|output| String::from_utf8(output.stdout).ok())
+        .unwrap_or("(unknown)".to_string());
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
