eb46c31db0e379b063e8e112cfc79c8350b36008
donkey_hotei <donkey-hotei@cryptolab.net>
2018-11-02 16:19:13 -0700

    handle any error raised when calling get_sockets_info

--- rita/src/rita_common/port_emissary/mod.rs
+++ rita/src/rita_common/port_emissary/mod.rs
@@ -2,31 +2,34 @@ extern crate netstat;
 
 use rita_common::port_emissary::netstat::*;
 
-struct PortEmissary {
-    free_ports: Vec<u16>,
-}
+/// Retrieves list of free, unused, ports.
+pub fn used_ports() -> Result<Vec<u16>, Error> {
+    let prot_flags = ProtocolFlags::UDP;
+    let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+    let sockets_info = match get_sockets_info(af_flags, prot_flags) {
+        Err(e) => return Err(e),
+        Ok(info) => info,
+    };
 
-impl PortEmissary {
-    fn new() -> PortEmissary {
-        let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
-        let sockets_info = get_sockets_info(af_flags, ProtocolFlags::UDP).unwrap();
+    let mut used_ports: Vec<u16> = Vec::new();
 
-        let mut used_ports: Vec<u16> = Vec::new();
-        for si in sockets_info {
-            match si.protocol_socket_info {
-                ProtocolSocketInfo::Udp(udp_si) => used_ports.push(udp_si.local_port),
-                _ => continue,
-            }
+    for si in sockets_info {
+        match si.protocol_socket_info {
+            ProtocolSocketInfo::Udp(udp_si) => used_ports.push(udp_si.local_port),
+            _ => continue,
         }
-
-        let free_ports: Vec<u16> = (0..65535).filter(|s| !used_ports.contains(s)).collect();
-
-        PortEmissary { free_ports }
     }
+
+    Ok(used_ports)
 }
 
+/// Retrieves list of free, unused, ports.
 pub fn free_ports() -> Vec<u16> {
-    PortEmissary::new().free_ports
+    if let Some(ports_inuse) = used_ports().ok() {
+        (0..65535).filter(|s| !ports_inuse.contains(s)).collect()
+    } else {
+        Vec::new()
+    }
 }
 
 #[test]
