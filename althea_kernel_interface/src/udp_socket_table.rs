87a02b36cd640cff7e768f838828c770e671f5fd
donkey_hotei <donkey-hotei@cryptolab.net>
2018-11-05 09:22:16 -0800

    introduce custom UdpTableError wrapper enum and use over &'static str

--- althea_kernel_interface/src/udp_socket_table.rs
+++ althea_kernel_interface/src/udp_socket_table.rs
@@ -1,41 +1,46 @@
-use super::KernelInterface;
+use failure::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::u16;
 
-use super::KernelInterfaceError;
-use failure::Error;
-
-/// Returns a kernel interface runtime error with the given message.
-fn runtime_error<T>(msg: &str) -> Result<T, Error> {
-    Err(KernelInterfaceError::RuntimeError(msg.to_string()).into())
+#[derive(Debug, Fail)]
+pub enum UdpTableError {
+    #[fail(display = "Parse Error: {:?}", _0)]
+    ParseError(String),
 }
 
 /// Helper function for parsing out port number from local_address column
-fn parse_local_port(s: &str) -> Result<u16, Error> {
+fn parse_local_port(s: &str) -> Result<u16, &'static str> {
     // second column in table contains local_address
     let local_addr = match s.split_whitespace().nth(1) {
         Some(addr) => addr,
-        None => return runtime_error("Error parsing local_address column!"),
+        None => return UdpTableError::ParseError("Error parsing local_address column!"),
     };
     // having a format like "00000000:14E9"
     let port = match local_addr.split(":").nth(1) {
         Some(port) => port,
-        None => return runtime_error("Error parsing local_address column!"),
+        None => return UdpTableError::ParseError("Error parsing local_address column!"),
     };
 
     match u16::from_str_radix(port, 16) {
         Ok(port) => Ok(port),
-        Err(_) => runtime_error("Error parsing port from local_address column!"),
+        Err(err) => {
+            return UdpTableError::ParseError("Error parsing port from local_address column!")
+        }
     }
 }
 
-impl KernelInterface {
-    fn read_udp_socket_table(&self) -> Result<String, Error> {
-        let mut f = File::open("/proc/net/udp")?;
-        let mut contents = String::new();
+/// Returns list of ports in use as seen in the UDP socket table (/proc/net/udp)
+fn used_ports() -> Result<Vec<u16>, &'static str> {
+    let mut f = match File::open("/proc/net/udp") {
+        Ok(file) => file,
+        Err(err) => return Err(err),
+    };
+    let mut udp_sockets_table = String::new();
 
-        f.read_to_string(&mut contents)?;
+    if f.read_to_string(&mut udp_sockets_table).is_err() {
+        return UdpTableError::ParseError("Error reading UDP socket table!");
+    };
 
         Ok(contents)
     }
@@ -47,11 +52,11 @@ impl KernelInterface {
 
         lines.next(); // advance iterator to skip header
 
-        let ports: Vec<u16> = lines
-            .take_while(|line| line.len() > 0)  // until end of the table is reached,
-            .map(|line| parse_local_port(line)) // parse each udp port,
-            .filter_map(Result::ok) // only taking those which parsed successfully
-            .collect();
+    let ports: Vec<u16> = lines
+        .take_while(|line| line.len() > 0)
+        .map(|line| parse_local_port(line))
+        .filter_map(Result::ok)
+        .collect();
 
         Ok(ports)
     }
