8515d93fab352dc76d1e33e9448b58fad5da2b61
donkey_hotei <donkey-hotei@cryptolab.net>
2018-11-05 09:45:09 -0800

    use already existing KernelInterfaceError enum

--- althea_kernel_interface/src/udp_socket_table.rs
+++ althea_kernel_interface/src/udp_socket_table.rs
@@ -1,32 +1,36 @@
-use failure::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::u16;
 
-#[derive(Debug, Fail)]
-pub enum UdpTableError {
-    #[fail(display = "Parse Error: {:?}", _0)]
-    ParseError(String),
-}
+use super::KernelInterfaceError;
+use failure::Error;
 
 /// Helper function for parsing out port number from local_address column
-fn parse_local_port(s: &str) -> Result<u16, &'static str> {
+fn parse_local_port(s: &str) -> Result<u16, Error> {
     // second column in table contains local_address
     let local_addr = match s.split_whitespace().nth(1) {
         Some(addr) => addr,
-        None => return UdpTableError::ParseError("Error parsing local_address column!"),
+        None => {
+            return Err(KernelInterfaceError::RuntimeError(
+                "Error parsing local_address column!",
+            ))
+        }
     };
     // having a format like "00000000:14E9"
     let port = match local_addr.split(":").nth(1) {
         Some(port) => port,
-        None => return UdpTableError::ParseError("Error parsing local_address column!"),
+        None => {
+            return Err(KernelInterfaceError::RuntimeError(
+                "Error parsing local_address column!",
+            ))
+        }
     };
 
     match u16::from_str_radix(port, 16) {
         Ok(port) => Ok(port),
-        Err(err) => {
-            return UdpTableError::ParseError("Error parsing port from local_address column!")
-        }
+        Err(err) => Err(KernelInterfaceError::RuntimeError(
+            "Error parsing port from local_address column!",
+        )),
     }
 }
 
@@ -39,7 +43,9 @@ fn used_ports() -> Result<Vec<u16>, &'static str> {
     let mut udp_sockets_table = String::new();
 
     if f.read_to_string(&mut udp_sockets_table).is_err() {
-        return UdpTableError::ParseError("Error reading UDP socket table!");
+        return Err(KernelInterfaceError::RuntimeError(
+            "Error reading UDP socket table!",
+        ));
     };
 
     let mut lines = udp_sockets_table.split("\n");
