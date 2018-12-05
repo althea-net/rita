5986cffce0f7e2269945e957bc240413314612c2
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-12-05 15:43:55 -0500

    Fix iptables counting rule location
    Previously we sould insert our forwarding rule to count traffic
    at the end of the chain. On desktop linux this works fine, but the
    chain on OpenWRT forwards early. Now we place the rule at the start

--- althea_kernel_interface/src/iptables.rs
+++ althea_kernel_interface/src/iptables.rs
@@ -6,12 +6,23 @@ impl KernelInterface {
     pub fn add_iptables_rule(&self, command: &str, rule: &[&str]) -> Result<(), Error> {
         assert!(rule.contains(&"-A") || rule.contains(&"-I"));
 
-        let check_rule: Vec<&str> = rule
-            .iter()
-            .map(|x| if x == &"-A" || x == &"-I" { "-C" } else { x })
-            .collect();
+        let mut new_command = Vec::new();
+        let mut i_pos_skip = None;
+        for i in 0..rule.len() {
+            if i_pos_skip.is_some() && i == i_pos_skip.unwrap() {
+                continue;
+            }
+            if rule[i] == "-I" {
+                new_command.push("-C");
+                i_pos_skip = Some(i + 2);
+            } else if rule[i] == "-A" {
+                new_command.push("-C");
+            } else {
+                new_command.push(rule[i]);
+            }
+        }
 
-        let check = self.run_command(command, &check_rule)?;
+        let check = self.run_command(command, &new_command)?;
 
         if !check.status.success() {
             self.run_command(command, rule)?;
