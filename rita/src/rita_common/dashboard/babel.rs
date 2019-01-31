960158bc82a6e6f32038d11e2957fb7bf598618d
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2019-01-31 08:09:20 -0500

    Remove uneeded futures from common dashboard

--- rita/src/rita_common/dashboard/babel.rs
+++ rita/src/rita_common/dashboard/babel.rs
@@ -1,22 +1,22 @@
 use super::*;
 
-pub fn get_local_fee(_req: HttpRequest) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
+pub fn get_local_fee(_req: HttpRequest) -> Result<HttpResponse, Error> {
     debug!("/local_fee GET hit");
     let mut ret = HashMap::new();
     ret.insert("local_fee", SETTING.get_payment().local_fee);
 
-    Box::new(future::ok(HttpResponse::Ok().json(ret)))
+    Ok(HttpResponse::Ok().json(ret))
 }
 
-pub fn get_metric_factor(_req: HttpRequest) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
+pub fn get_metric_factor(_req: HttpRequest) -> Result<HttpResponse, Error> {
     debug!("/local_fee GET hit");
     let mut ret = HashMap::new();
     ret.insert("metric_factor", SETTING.get_network().metric_factor);
 
-    Box::new(future::ok(HttpResponse::Ok().json(ret)))
+    Ok(HttpResponse::Ok().json(ret))
 }
 
-pub fn set_local_fee(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
+pub fn set_local_fee(path: Path<u32>) -> Result<HttpResponse, Error> {
     let new_fee = path.into_inner();
     debug!("/local_fee/{} POST hit", new_fee);
     let mut ret = HashMap::<String, String>::new();
@@ -35,11 +35,9 @@ pub fn set_local_fee(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse, Err
             );
             ret.insert("rust_error".to_owned(), format!("{:?}", e));
 
-            return Box::new(future::ok(
-                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
-                    .into_builder()
-                    .json(ret),
-            ));
+            return Ok(HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+                .into_builder()
+                .json(ret));
         }
     };
 
@@ -50,11 +48,9 @@ pub fn set_local_fee(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse, Err
         ret.insert("error".to_owned(), "Could not connect to Babel".to_owned());
         ret.insert("rust_error".to_owned(), format!("{:?}", e));
 
-        return Box::new(future::ok(
-            HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
-                .into_builder()
-                .json(ret),
-        ));
+        return Ok(HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            .into_builder()
+            .json(ret));
     }
 
     if let Err(e) = babel.set_local_fee(new_fee) {
@@ -65,11 +61,9 @@ pub fn set_local_fee(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse, Err
         );
         ret.insert("rust_error".to_owned(), format!("{:?}", e));
 
-        return Box::new(future::ok(
-            HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
-                .into_builder()
-                .json(ret),
-        ));
+        return Ok(HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            .into_builder()
+            .json(ret));
     };
 
     // Set the value in settings only after Babel successfuly accepts the passed value
@@ -77,7 +71,7 @@ pub fn set_local_fee(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse, Err
 
     // try and save the config and fail if we can't
     if let Err(e) = SETTING.write().unwrap().write(&ARGS.flag_config) {
-        return Box::new(future::err(e));
+        return Err(e);
     }
 
     if new_fee == 0 {
@@ -85,10 +79,10 @@ pub fn set_local_fee(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse, Err
         ret.insert("warning".to_owned(), "THIS NODE IS GIVING BANDWIDTH AWAY FOR FREE. PLEASE SET local_fee TO A NON-ZERO VALUE TO DISABLE THIS WARNING.".to_owned());
     }
 
-    Box::new(future::ok(HttpResponse::Ok().json(ret)))
+    Ok(HttpResponse::Ok().json(ret))
 }
 
-pub fn set_metric_factor(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
+pub fn set_metric_factor(path: Path<u32>) -> Result<HttpResponse, Error> {
     let new_factor = path.into_inner();
     debug!("/metric_factor/{} POST hit", new_factor);
     let mut ret = HashMap::<String, String>::new();
@@ -107,11 +101,9 @@ pub fn set_metric_factor(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse,
             );
             ret.insert("rust_error".to_owned(), format!("{:?}", e));
 
-            return Box::new(future::ok(
-                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
-                    .into_builder()
-                    .json(ret),
-            ));
+            return Ok(HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+                .into_builder()
+                .json(ret));
         }
     };
 
@@ -122,11 +114,9 @@ pub fn set_metric_factor(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse,
         ret.insert("error".to_owned(), "Could not connect to Babel".to_owned());
         ret.insert("rust_error".to_owned(), format!("{:?}", e));
 
-        return Box::new(future::ok(
-            HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
-                .into_builder()
-                .json(ret),
-        ));
+        return Ok(HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            .into_builder()
+            .json(ret));
     }
 
     if let Err(e) = babel.set_metric_factor(new_factor) {
@@ -137,11 +127,9 @@ pub fn set_metric_factor(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse,
         );
         ret.insert("rust_error".to_owned(), format!("{:?}", e));
 
-        return Box::new(future::ok(
-            HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
-                .into_builder()
-                .json(ret),
-        ));
+        return Ok(HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            .into_builder()
+            .json(ret));
     };
 
     // Set the value in settings only after Babel successfuly accepts the passed value
@@ -149,7 +137,7 @@ pub fn set_metric_factor(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse,
 
     // try and save the config and fail if we can't
     if let Err(e) = SETTING.write().unwrap().write(&ARGS.flag_config) {
-        return Box::new(future::err(e));
+        return Err(e);
     }
 
     if new_factor == 0 {
@@ -157,5 +145,5 @@ pub fn set_metric_factor(path: Path<u32>) -> Box<dyn Future<Item = HttpResponse,
         ret.insert("warning".to_owned(), "THIS NODE DOESN'T PAY ATTENTION TO ROUTE QUALITY - IT'LL CHOOSE THE CHEAPEST ROUTE EVEN IF IT'S THE WORST LINK AROUND. PLEASE SET metric_factor TO A NON-ZERO VALUE TO DISABLE THIS WARNING.".to_owned());
     }
 
-    Box::new(future::ok(HttpResponse::Ok().json(ret)))
+    Ok(HttpResponse::Ok().json(ret))
 }
--- rita/src/rita_common/dashboard/pricing.rs
+++ rita/src/rita_common/dashboard/pricing.rs
@@ -5,14 +5,14 @@ pub fn auto_pricing_status(_req: HttpRequest) -> Result<Json<bool>, Error> {
     Ok(Json(SETTING.get_payment().price_oracle_enabled))
 }
 
-pub fn set_auto_pricing(path: Path<bool>) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
+pub fn set_auto_pricing(path: Path<bool>) -> Result<HttpResponse, Error> {
     let value = path.into_inner();
     debug!("Set Auto pricing enabled hit!");
     SETTING.get_payment_mut().price_oracle_enabled = value;
 
     // try and save the config and fail if we can't
     if let Err(e) = SETTING.write().unwrap().write(&ARGS.flag_config) {
-        return Box::new(future::err(e));
+        return Err(e);
     }
-    Box::new(future::ok(HttpResponse::Ok().json(())))
+    Ok(HttpResponse::Ok().json(()))
 }
