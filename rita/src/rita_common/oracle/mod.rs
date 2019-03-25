3092581bc81f74bb0a94323fa115f294d6920c39
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2019-03-25 11:06:37 -0400

    Add fixed low balance treshold
    While having a dynamic payment limit based on gas prices works quite
    well for payments it's not a good thing to base a low balance system off
    of. What will happen is that the system will reach a state where as
    gas prices fluctuate the user goes on and off of the low balance mode
    with each block.
    
    A fixed value resolves this problem, but will require future adjustment
    and probably oracalization

--- rita/src/rita_common/oracle/mod.rs
+++ rita/src/rita_common/oracle/mod.rs
@@ -329,13 +329,9 @@ fn update_our_price() {
 /// A very simple function placed here for convinence that indicates
 /// if the system should go into low balance mode
 pub fn low_balance() -> bool {
-    let payment_threshold = match SETTING.get_payment().pay_threshold.to_uint256() {
-        Some(val) => val,
-        None => Uint256::zero(),
-    };
     let balance = SETTING.get_payment().balance.clone();
-    // might increase this in the future
-    const MULTIPLIER: u8 = 4;
+    // TODO this needs to be configured/oracalized
+    const THRESHOLD: u128 = 10_000_000_000_000_000;
 
-    balance < (payment_threshold * MULTIPLIER.into())
+    balance < THRESHOLD.into()
 }
