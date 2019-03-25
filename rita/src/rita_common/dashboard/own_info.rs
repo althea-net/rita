c639890b13937748a5a63bb4ccbb4fe49331533a
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2019-03-25 11:08:18 -0400

    Bump for rc9

--- rita/src/rita_common/dashboard/own_info.rs
+++ rita/src/rita_common/dashboard/own_info.rs
@@ -6,7 +6,7 @@ use clarity::Address;
 use failure::Error;
 use num256::{Int256, Uint256};
 
-pub static READABLE_VERSION: &str = "Beta 3 RC8";
+pub static READABLE_VERSION: &str = "Beta 3 RC9";
 
 #[derive(Serialize)]
 pub struct OwnInfo {
