d7a2806524d9e866fea96f343e4365d44c8f227a
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2019-04-05 19:48:33 -0400

    Hash and salt the dashboard password
    Since we're using httpauth this can't be perfect, but now the router
    at least secures the password at rest

--- rita/src/rita_common/dashboard/auth.rs
+++ rita/src/rita_common/dashboard/auth.rs
@@ -5,6 +5,7 @@ use ::actix_web::{HttpResponse, Json};
 use ::settings::FileWrite;
 use failure::Error;
 use settings::RitaCommonSettings;
+use sha3::{Digest, Sha3_256};
 
 #[derive(Serialize, Deserialize, Default, Clone, Debug)]
 pub struct RouterPassword {
@@ -15,7 +16,10 @@ pub fn set_pass(router_pass: Json<RouterPassword>) -> Result<HttpResponse, Error
     debug!("/router/password hit with {:?}", router_pass);
     let router_pass = router_pass.into_inner();
     // scoped to drop the write reference before we write to the disk
-    SETTING.get_network_mut().rita_dashboard_password = Some(router_pass.password.clone());
+    let mut hasher = Sha3_256::new();
+    hasher.input(router_pass.password.clone() + "RitaSalt");
+    let hashed_pass = String::from_utf8(hasher.result().to_vec())?;
+    SETTING.get_network_mut().rita_dashboard_password = Some(hashed_pass);
     // try and save the config and fail if we can't
     if let Err(e) = SETTING.write().unwrap().write(&ARGS.flag_config) {
         return Err(e);
