ddd1c2db230b74e9c95af359e03da94ae2dd3f8d
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2019-04-16 09:26:25 -0400

    Clean up for Client traffic watcher
    fprtt was computed and generally not being used awkwardly in the middle
    of the client billing, that's been lifted out into it's own function
    in rita_loop as well as the route finding operation.

--- rita/src/rita_client/rita_loop/mod.rs
+++ rita/src/rita_client/rita_loop/mod.rs
@@ -4,17 +4,18 @@
 //! This loop manages exit signup based on the settings configuration state and deploys an exit vpn
 //! tunnel if the signup was successful on the selected exit.
 
-use std::time::{Duration, Instant};
-
+use crate::rita_client::exit_manager::ExitManager;
+use crate::SETTING;
 use actix::{
     Actor, ActorContext, Addr, Arbiter, AsyncContext, Context, Handler, Message, Supervised,
     SystemService,
 };
-use futures::future::Future;
-
-use crate::rita_client::exit_manager::ExitManager;
-
+use althea_types::RTTimestamps;
 use failure::Error;
+use futures::future::Future;
+use reqwest;
+use settings::client::RitaClientSettings;
+use std::time::{Duration, Instant};
 
 #[derive(Default)]
 pub struct RitaLoop;
@@ -79,4 +80,40 @@ impl Handler<Tick> for RitaLoop {
         );
         Ok(())
     }
+}
+
+pub fn _compute_verification_rtt() -> Result<RTTimestamps, Error> {
+    let exit = match SETTING.get_exit_client().get_current_exit() {
+        Some(current_exit) => current_exit.clone(),
+        None => {
+            return Err(format_err!(
+                "No current exit even though an exit route is present"
+            ));
+        }
+    };
+
+    let client = reqwest::Client::builder()
+        .timeout(Duration::from_secs(1))
+        .build()?;
+
+    let timestamps: RTTimestamps = client
+        .get(&format!(
+            "http://[{}]:{}/rtt",
+            exit.id.mesh_ip, exit.registration_port
+        ))
+        .send()?
+        .json()?;
+
+    let exit_rx = timestamps.exit_rx;
+    let exit_tx = timestamps.exit_tx;
+    let client_tx = Instant::now();
+    let client_rx = Instant::now();
+
+    let inner_rtt = client_rx.duration_since(client_tx) - exit_tx.duration_since(exit_rx)?;
+    let inner_rtt_millis =
+        inner_rtt.as_secs() as f32 * 1000.0 + inner_rtt.subsec_nanos() as f32 / 1_000_000.0;
+    //                        secs -> millis                            nanos -> millis
+
+    info!("Computed RTTs: inner {}ms", inner_rtt_millis);
+    Ok(timestamps)
 }
