11efa4a7b5060b17da91dd41153cf75110af369b
Adam Soltys <asoltys@gmail.com>
2019-07-03 12:22:12 -0700

    Add /router/reboot endpoint

--- rita/src/rita_client/dashboard/router.rs
+++ rita/src/rita_client/dashboard/router.rs
@@ -1,6 +1,15 @@
 use crate::KI;
-use ::actix_web::{HttpRequest, HttpResponse};
+use actix_web::{HttpRequest, HttpResponse};
 use failure::Error;
+
+pub fn reboot_router(_req: HttpRequest) -> Result<HttpResponse, Error> {
+    if KI.is_openwrt() {
+        KI.run_command("reboot", &[])?;
+        Ok(HttpResponse::Ok().json(()))
+    } else {
+        Ok(HttpResponse::Ok().json("This isn't an OpenWRT device, doing nothing"))
+    }
+}
 
 pub fn update_router(_req: HttpRequest) -> Result<HttpResponse, Error> {
     if KI.is_openwrt() {
