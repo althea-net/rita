9c3723dc28b1c8bb8ee46fde21ac6eb65f8813ed
Justin Kilpatrick <justin@althea.net>
2019-07-30 10:40:52 -0400

    Setup new oracle with existing DAOs

--- rita/src/client.rs
+++ rita/src/client.rs
@@ -169,6 +169,13 @@ fn main() {
         env_logger::init();
     }
 
+    // remove in beta 9 as this will already be set propertly
+    let mut dao_settings = SETTING.get_dao_mut();
+    if let Some(address) = dao_settings.dao_addresses.iter().last() {
+        dao_settings.oracle_url = Some(format!("https://updates.althea.net/{}", address));
+    }
+    drop(dao_settings);
+
     // remove in beta 9 where this info will be imported from the dao address oracle.
     let mut payment_settings = SETTING.get_payment_mut();
     payment_settings.withdraw_chain = payment_settings.system_chain;
