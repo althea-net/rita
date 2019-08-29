8742121d63e1aa995d96eaa6e7fb4302f15675b8
Justin Kilpatrick <justin@althea.net>
2019-08-29 07:55:40 -0400

    Upgrade to Beta 8 RC 2

--- rita/src/rita_common/dashboard/own_info.rs
+++ rita/src/rita_common/dashboard/own_info.rs
@@ -6,7 +6,7 @@ use clarity::Address;
 use failure::Error;
 use num256::{Int256, Uint256};
 
-pub static READABLE_VERSION: &str = "Beta 8 RC1";
+pub static READABLE_VERSION: &str = "Beta 8 RC2";
 
 #[derive(Serialize)]
 pub struct OwnInfo {
