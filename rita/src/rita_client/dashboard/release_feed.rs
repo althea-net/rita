4a8d2c2af472fa9ed21bc695828aa9a66b2a18bc
Justin Kilpatrick <justin@althea.net>
2019-09-05 14:38:03 -0400

    Add easy remote access toggle

--- rita/src/rita_client/dashboard/release_feed.rs
+++ rita/src/rita_client/dashboard/release_feed.rs
@@ -1,13 +1,12 @@
+use crate::rita_client::dashboard::get_lines;
+use crate::rita_client::dashboard::write_out;
+use crate::KI;
 use actix_web::http::StatusCode;
 use actix_web::HttpRequest;
 use actix_web::HttpResponse;
 use actix_web::Path;
 use failure::Error;
 use regex::Regex;
-use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
-use std::io::Write;
 use std::str::FromStr;
 
 static CUSTOMFEEDS: &str = "/etc/opkg/customfeeds.conf";
@@ -32,32 +31,11 @@ impl FromStr for ReleaseStatus {
     }
 }
 
-fn get_lines(filename: &str) -> Result<Vec<String>, Error> {
-    let f = File::open(filename)?;
-    let file = BufReader::new(&f);
-    let mut out_lines = Vec::new();
-    for line in file.lines() {
-        match line {
-            Ok(val) => out_lines.push(val),
-            Err(_) => break,
-        }
-    }
-
-    Ok(out_lines)
-}
-
-fn write_out(content: Vec<String>) -> Result<(), Error> {
-    // overwrite the old version
-    let mut file = File::create(CUSTOMFEEDS)?;
-    let mut final_ouput = String::new();
-    for item in content {
-        final_ouput += &format!("{}\n", item);
-    }
-    file.write_all(final_ouput.as_bytes())?;
-    Ok(())
-}
-
 pub fn get_release_feed(_req: HttpRequest) -> Result<HttpResponse, Error> {
+    if !KI.is_openwrt() {
+        return Ok(HttpResponse::new(StatusCode::BAD_REQUEST));
+    }
+
     let lines = get_lines(CUSTOMFEEDS)?;
     for line in lines.iter() {
         if line.contains(&"/rc/".to_string()) && line.contains(&FEED_NAME.to_string()) {
@@ -70,6 +48,10 @@ pub fn get_release_feed(_req: HttpRequest) -> Result<HttpResponse, Error> {
 }
 
 pub fn set_release_feed(path: Path<String>) -> Result<HttpResponse, Error> {
+    if !KI.is_openwrt() {
+        return Ok(HttpResponse::new(StatusCode::BAD_REQUEST));
+    }
+
     let val = path.into_inner().parse();
     if val.is_err() {
         return Ok(HttpResponse::new(StatusCode::BAD_REQUEST)
@@ -103,7 +85,7 @@ pub fn set_release_feed(path: Path<String>) -> Result<HttpResponse, Error> {
             *line = src_line;
         }
     }
-    write_out(lines)?;
+    write_out(CUSTOMFEEDS, lines)?;
 
     Ok(HttpResponse::Ok().json(()))
 }
