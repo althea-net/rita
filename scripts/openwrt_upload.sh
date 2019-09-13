9adda6c137609d7560fdf46e9a3930b2965b9003
Adam Soltys <asoltys@gmail.com>
2019-09-13 16:53:19 -0700

    Revert change to openwrt upload script

--- scripts/openwrt_upload.sh
+++ scripts/openwrt_upload.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -eux
-export TARGET=mvebu
-export TRIPLE=armv7-unknown-linux-musleabihf
+export TARGET=mips
+export TRIPLE=mips-unknown-linux-musl
 export ROUTER_IP=192.168.10.1
 bash scripts/openwrt_build_$TARGET.sh $@
 set +e
 ssh root@$ROUTER_IP killall -9 rita
 set -e
-scp target/$TRIPLE/release/rita root@$ROUTER_IP:/usr/sbin/rita
-ssh root@$ROUTER_IP NO_REMOTE_LOG=true RUST_BACKTRACE=FULL RUST_LOG=TRACE /usr/sbin/rita --config=/etc/rita.toml --platform=linux &> out.log
+scp target/$TRIPLE/release/rita root@$ROUTER_IP:/tmp/rita
+ssh root@$ROUTER_IP NO_REMOTE_LOG=true RUST_BACKTRACE=FULL RUST_LOG=TRACE /tmp/rita --config=/etc/rita.toml --platform=linux &> out.log
