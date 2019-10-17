982e9ed231e576d012aa22d05496e1034e8609db
Justin Kilpatrick <justin@althea.net>
2019-10-17 08:17:43 -0400

    Update for Beta 9 RC4

--- rita/src/rita_common/dashboard/own_info.rs
+++ rita/src/rita_common/dashboard/own_info.rs
@@ -6,7 +6,7 @@ use failure::Error;
 use num256::{Int256, Uint256};
 use settings::RitaCommonSettings;
 
-pub static READABLE_VERSION: &str = "Beta 9 RC3";
+pub static READABLE_VERSION: &str = "Beta 9 RC4";
 
 #[derive(Serialize)]
 pub struct OwnInfo {
