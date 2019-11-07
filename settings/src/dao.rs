cd1db7d13afd4486d314807fdec5c2f4ed73561d
Justin Kilpatrick <justin@althea.net>
2019-11-07 10:10:44 -0600

    Only one node list
    Originally we where considering the problem of having Aragon on one blockchain and our
    payments on the other. That's not going to be implemented ofr the forseeable future and
    for the time being I'd prefer to have the reduced complexity

--- settings/src/dao.rs
+++ settings/src/dao.rs
@@ -1,13 +1,6 @@
 use clarity::Address;
 use num256::Uint256;
 
-fn default_node_list() -> Vec<String> {
-    vec![
-        "https://eth.althea.org:443".to_string(),
-        "https://mainnet.infura.io/v3/6b080f02d7004a8394444cdf232a7081".to_string(),
-    ]
-}
-
 fn default_dao_address() -> Vec<Address> {
     Vec::new()
 }
@@ -21,12 +14,6 @@ fn default_oracle_url() -> Option<String> {
 
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, Default)]
 pub struct SubnetDAOSettings {
-    /// A list of nodes to query for blockchain data
-    /// this is kept seperate from the version for payment settings node
-    /// list in order to allow for the DAO and payments to exist on different
-    /// chains, provided in name:port format
-    #[serde(default = "default_node_list")]
-    pub node_list: Vec<String>,
     /// List of subnet DAO's to which we are a member
     #[serde(default = "default_dao_address")]
     pub dao_addresses: Vec<Address>,
