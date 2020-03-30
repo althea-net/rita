3443f53a5096da4b2e64bed16eaf41d25fec7f28
Justin Kilpatrick <justin@althea.net>
2020-03-30 14:30:56 -0400

    Improve Ip Increment logic
    This modifies the ip increment logic in the utils file to use the
    integrated rust functions for byte conversion to basic integer types.
    
    This results in a much simpiler algorithm across the board and improvements
    in correctness and functionality. For example there is no more limitation to
    use a factor of 8 as a subnet size.

--- rita/src/rita_common/utils/ip_increment.rs
+++ rita/src/rita_common/utils/ip_increment.rs
@@ -1,116 +1,112 @@
 use failure::Error;
 use std::net::IpAddr;
 use std::net::Ipv4Addr;
+use std::net::Ipv6Addr;
 
 #[allow(dead_code)]
 pub fn incrementv4(address: Ipv4Addr, netmask: u8) -> Result<Ipv4Addr, Error> {
-    assert_eq!(netmask % 8, 0);
-    // the number of bytes we can cover using this netmask
-    let bytes_to_modify = ((32 - netmask) + 7) / 8;
-    assert!(netmask <= 32);
-    assert!(bytes_to_modify <= 4);
-    assert!(bytes_to_modify > 0);
-
-    let mut carry = false;
-    let mut oct = address.octets();
-    for i in (3 - (bytes_to_modify)..4).rev() {
-        let index = i as usize;
-        if i == (4 - bytes_to_modify) && oct[index] == 255 && carry {
-            bail!("Ip space in the netmask has been exhausted!");
-        }
-
-        if oct[index] == 255 {
-            oct[index] = 0;
-            carry = true;
-            continue;
-        }
-
-        if carry {
-            oct[index] += 1;
-            return Ok(oct.into());
-        }
-
-        oct[index] += 1;
-        return Ok(oct.into());
+    let byte_vec = u32::from_be_bytes(address.octets());
+    let mut mask = 0;
+    for i in 0..netmask {
+        mask = flip_bit_at_index_u32(mask, i);
     }
-    bail!("No more ip address space!")
+    let new_ip = byte_vec + 1;
+    if new_ip & !mask == 0 {
+        bail!("Address space exhausted!");
+    }
+
+    let new_ip: Ipv4Addr = new_ip.to_be_bytes().into();
+    Ok(new_ip)
+}
+
+#[allow(dead_code)]
+pub fn incrementv6(address: Ipv6Addr, netmask: u8) -> Result<Ipv6Addr, Error> {
+    let byte_vec = u128::from_be_bytes(address.octets());
+    let mut mask = 0;
+    for i in 0..netmask {
+        mask = flip_bit_at_index_u128(mask, i);
+    }
+    let new_ip = byte_vec + 1;
+    if new_ip & !mask == 0 {
+        bail!("Address space exhausted!");
+    }
+
+    let new_ip: Ipv6Addr = new_ip.to_be_bytes().into();
+    Ok(new_ip)
+}
+
+/// flips the bit at the given index assuming bigendian counting
+fn flip_bit_at_index_u32(input: u32, bit: u8) -> u32 {
+    let mask = 1;
+    let shift = 31 - bit;
+    let mask = mask << shift;
+    input ^ mask
+}
+
+/// flips the bit at the given index assuming bigendian counting
+fn flip_bit_at_index_u128(input: u128, bit: u8) -> u128 {
+    let mask = 1;
+    let shift = 127 - bit;
+    let mask = mask << shift;
+    input ^ mask
 }
 
 /// adds one to whole netmask ip addresses
 #[allow(dead_code)]
 pub fn increment(address: IpAddr, netmask: u8) -> Result<IpAddr, Error> {
     assert_eq!(netmask % 8, 0);
-    // same algorithm for either path, couldn't converge the codepaths
-    // without having to play with slices for oct
     match address {
-        IpAddr::V4(address) => {
-            // the number of bytes we can cover using this netmask
-            let bytes_to_modify = ((32 - netmask) + 7) / 8;
-            assert!(netmask <= 32);
-            assert!(bytes_to_modify <= 4);
-            assert!(bytes_to_modify > 0);
-
-            let mut carry = false;
-            let mut oct = address.octets();
-            for i in (3 - (bytes_to_modify)..4).rev() {
-                let index = i as usize;
-                if i == (4 - bytes_to_modify) && oct[index] == 255 && carry {
-                    bail!("Ip space in the netmask has been exhausted!");
-                }
-
-                if oct[index] == 255 {
-                    oct[index] = 0;
-                    carry = true;
-                    continue;
-                }
-
-                if carry {
-                    oct[index] += 1;
-                    return Ok(oct.into());
-                }
-
-                oct[index] += 1;
-                return Ok(oct.into());
-            }
-            bail!("No more ip address space!")
-        }
-        IpAddr::V6(address) => {
-            // the number of bytes we can cover using this netmask
-            let bytes_to_modify = ((128 - netmask) + 7) / 8;
-            assert!(netmask <= 128);
-            assert!(bytes_to_modify <= 16);
-            assert!(bytes_to_modify > 0);
-
-            let mut carry = false;
-            let mut oct = address.octets();
-            for i in ((16 - bytes_to_modify)..16).rev() {
-                let index = i as usize;
-                if i == (15 - bytes_to_modify) && oct[index] == 255 && carry {
-                    bail!("Ip space in the netmask has been exhausted!");
-                }
-
-                if oct[index] == 255 {
-                    oct[index] = 0;
-                    carry = true;
-                    continue;
-                }
-
-                if carry {
-                    oct[index] += 1;
-                    return Ok(oct.into());
-                }
-
-                oct[index] += 1;
-                return Ok(oct.into());
-            }
-            bail!("No more ip address space!")
-        }
+        IpAddr::V4(address) => match incrementv4(address, netmask) {
+            Ok(addr) => Ok(addr.into()),
+            Err(e) => Err(e),
+        },
+        IpAddr::V6(address) => match incrementv6(address, netmask) {
+            Ok(addr) => Ok(addr.into()),
+            Err(e) => Err(e),
+        },
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn flip_start_u32() {
+        let res = flip_bit_at_index_u32(0, 0);
+        assert_eq!(res, u32::pow(2, 31))
+    }
+
+    #[test]
+    fn flip_end_u32() {
+        let res = flip_bit_at_index_u32(0, 31);
+        assert_eq!(res, 1)
+    }
+
+    #[test]
+    fn flip_middle_u32() {
+        let res = flip_bit_at_index_u32(0, 15);
+        assert_eq!(res, u32::pow(2, 16))
+    }
+
+    #[test]
+    fn flip_start_u128() {
+        let res = flip_bit_at_index_u128(0, 0);
+        assert_eq!(res, u128::pow(2, 127))
+    }
+
+    #[test]
+    fn flip_end_u128() {
+        let res = flip_bit_at_index_u128(0, 127);
+        assert_eq!(res, 1)
+    }
+
+    #[test]
+    fn flip_middle_u128() {
+        let res = flip_bit_at_index_u128(0, 111);
+        assert_eq!(res, u128::pow(2, 16))
+    }
+
     #[test]
     fn increment_basic_v4() {
         let addr1: IpAddr = [0, 0, 0, 0].into();
@@ -126,8 +122,14 @@ mod tests {
     }
     #[test]
     fn increment_out_of_bounds_simple_v4() {
-        let addr1: IpAddr = [0, 0, 255, 255].into();
-        assert!(increment(addr1, 16).is_err());
+        let addr1: IpAddr = [0, 0, 0, 255].into();
+        assert!(increment(addr1, 24).is_err());
+    }
+
+    #[test]
+    fn increment_out_of_bounds_simple_v4_specific() {
+        let addr1: Ipv4Addr = [0, 0, 0, 255].into();
+        assert!(incrementv4(addr1, 24).is_err());
     }
 
     #[test]
