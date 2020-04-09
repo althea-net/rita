ef9be5ae58c23a440a200b76f91242243fabc3c8
Justin Kilpatrick <justin@althea.net>
2020-04-09 07:48:36 -0400

    Make Rita loop it's own thread
    I'm hoping this reduces some of the scale/blocking problems we've been
    seeing over on the portland exit.

--- rita/src/rita_exit/rita_loop/mod.rs
+++ rita/src/rita_exit/rita_loop/mod.rs
@@ -28,10 +28,7 @@ use crate::rita_exit::network_endpoints::*;
 use crate::rita_exit::traffic_watcher::{TrafficWatcher, Watch};
 use crate::KI;
 use crate::SETTING;
-use actix::{
-    Actor, ActorContext, Addr, Arbiter, AsyncContext, Context, Handler, Message, Supervised,
-    SystemService,
-};
+use actix::{Arbiter, SystemService};
 use actix_web::http::Method;
 use actix_web::{server, App};
 use althea_kernel_interface::ExitClient;
@@ -39,149 +36,96 @@ use babel_monitor::open_babel_stream;
 use babel_monitor::parse_routes;
 use babel_monitor::start_connection;
 use diesel::query_dsl::RunQueryDsl;
-use diesel::r2d2::ConnectionManager;
-use diesel::r2d2::PooledConnection;
-use diesel::PgConnection;
 use exit_db::models;
-use failure::Error;
 use futures01::future::Future;
 use settings::exit::RitaExitSettings;
 use settings::RitaCommonSettings;
-use std::collections::HashMap;
 use std::collections::HashSet;
-use std::net::IpAddr;
+use std::thread;
 use std::time::Duration;
 use std::time::Instant;
 use tokio::util::FutureExt;
 
 // the speed in seconds for the exit loop
 pub const EXIT_LOOP_SPEED: u64 = 5;
+pub const EXIT_LOOP_SPEED_DURATION: Duration = Duration::from_secs(EXIT_LOOP_SPEED);
 pub const EXIT_LOOP_TIMEOUT: Duration = Duration::from_secs(4);
 
-#[derive(Default)]
-pub struct RitaLoop {
-    /// a simple cache to prevent regularly asking Maxmind for the same geoip data
-    pub geoip_cache: HashMap<IpAddr, String>,
-    /// a cache of what tunnels we had setup last round, used to prevent extra setup ops
-    pub wg_clients: HashSet<ExitClient>,
-}
+pub fn start_rita_exit_loop() {
+    setup_exit_wg_tunnel();
+    // a cache of what tunnels we had setup last round, used to prevent extra setup ops
+    let mut wg_clients: HashSet<ExitClient> = HashSet::new();
+    thread::spawn(move || {
+        loop {
+            // opening a database connection takes at least several milliseconds, as the database server
+            // may be across the country, so to save on back and forth we open on and reuse it as much
+            // as possible
+            if let Ok(conn) = get_database_connection().timeout(EXIT_LOOP_TIMEOUT).wait() {
+                let start = Instant::now();
+                use exit_db::schema::clients::dsl::clients;
+                let babel_port = SETTING.get_network().babel_port;
+                info!("Exit tick!");
 
-impl Actor for RitaLoop {
-    type Context = Context<Self>;
+                if let Ok(clients_list) = clients.load::<models::Client>(&conn) {
+                    let ids = clients_to_ids(clients_list.clone());
 
-    fn started(&mut self, ctx: &mut Context<Self>) {
-        info!("exit loop started");
-        setup_exit_wg_tunnel();
-        ctx.run_interval(Duration::from_secs(EXIT_LOOP_SPEED), move |_act, ctx| {
-            let addr: Addr<Self> = ctx.address();
-            Arbiter::spawn(get_database_connection().then(move |database| {
-                match database {
-                    Ok(database) => addr.do_send(Tick(database)),
-                    Err(e) => error!("Could not reach database for Rita sync loop! {:?}", e),
-                }
-                Ok(())
-            }));
-        });
-    }
-}
+                    // watch and bill for traffic
+                    Arbiter::spawn(
+                        open_babel_stream(babel_port)
+                            .from_err()
+                            .and_then(|stream| {
+                                start_connection(stream).and_then(|stream| {
+                                    parse_routes(stream).and_then(|routes| {
+                                        TrafficWatcher::from_registry().do_send(Watch {
+                                            users: ids,
+                                            routes: routes.1,
+                                        });
+                                        Ok(())
+                                    })
+                                })
+                            })
+                            .timeout(EXIT_LOOP_TIMEOUT)
+                            .then(|ret| {
+                                if let Err(e) = ret {
+                                    error!("Failed to watch Exit traffic with {:?}", e)
+                                }
+                                Ok(())
+                            }),
+                    );
 
-impl SystemService for RitaLoop {}
-impl Supervised for RitaLoop {
-    fn restarting(&mut self, _ctx: &mut Context<RitaLoop>) {
-        error!("Rita Exit loop actor died! recovering!");
-    }
-}
-
-/// Used to test actor respawning
-pub struct Crash;
-
-impl Message for Crash {
-    type Result = Result<(), Error>;
-}
-
-impl Handler<Crash> for RitaLoop {
-    type Result = Result<(), Error>;
-    fn handle(&mut self, _: Crash, ctx: &mut Context<Self>) -> Self::Result {
-        ctx.stop();
-        Ok(())
-    }
-}
-
-pub struct Tick(PooledConnection<ConnectionManager<PgConnection>>);
-
-impl Message for Tick {
-    type Result = Result<(), Error>;
-}
-
-impl Handler<Tick> for RitaLoop {
-    type Result = Result<(), Error>;
-    fn handle(&mut self, msg: Tick, _ctx: &mut Context<Self>) -> Self::Result {
-        let start = Instant::now();
-        use exit_db::schema::clients::dsl::clients;
-        let babel_port = SETTING.get_network().babel_port;
-        info!("Exit tick!");
-
-        // opening a database connection takes at least several milliseconds, as the database server
-        // may be across the country, so to save on back and forth we open on and reuse it as much
-        // as possible
-        let conn = msg.0;
-
-        let clients_list = clients.load::<models::Client>(&conn)?;
-        let ids = clients_to_ids(clients_list.clone());
-
-        // watch and bill for traffic
-        Arbiter::spawn(
-            open_babel_stream(babel_port)
-                .from_err()
-                .and_then(|stream| {
-                    start_connection(stream).and_then(|stream| {
-                        parse_routes(stream).and_then(|routes| {
-                            TrafficWatcher::from_registry().do_send(Watch {
-                                users: ids,
-                                routes: routes.1,
-                            });
-                            Ok(())
-                        })
-                    })
-                })
-                .timeout(EXIT_LOOP_TIMEOUT)
-                .then(|ret| {
-                    if let Err(e) = ret {
-                        error!("Failed to watch Exit traffic with {:?}", e)
+                    // Create and update client tunnels
+                    match setup_clients(&clients_list, &wg_clients) {
+                        Ok(new_wg_clients) => wg_clients = new_wg_clients,
+                        Err(e) => error!("Setup clients failed with {:?}", e),
                     }
-                    Ok(())
-                }),
-        );
 
-        // Create and update client tunnels
-        match setup_clients(&clients_list, &self.wg_clients) {
-            Ok(wg_clients) => self.wg_clients = wg_clients,
-            Err(e) => error!("Setup clients failed with {:?}", e),
+                    // find users that have not been active within the configured time period
+                    // and remove them from the db
+                    let res = cleanup_exit_clients(&clients_list, &conn);
+                    if res.is_err() {
+                        error!("Exit client cleanup failed with {:?}", res);
+                    }
+
+                    // Make sure no one we are setting up is geoip unauthorized
+                    if !SETTING.get_allowed_countries().is_empty() {
+                        Arbiter::spawn(validate_clients_region(clients_list.clone()));
+                    }
+
+                    // handle enforcement on client tunnels by querying debt keeper
+                    // this consumes client list, you can move it up in exchange for a clone
+                    Arbiter::spawn(enforce_exit_clients(clients_list));
+
+                    info!(
+                        "Completed Rita sync loop in {}s {}ms, all vars should be dropped",
+                        start.elapsed().as_secs(),
+                        start.elapsed().subsec_millis(),
+                    );
+                }
+            }
+            // sleep for 5 seconds
+            thread::sleep(EXIT_LOOP_SPEED_DURATION);
         }
-
-        // find users that have not been active within the configured time period
-        // and remove them from the db
-        let res = cleanup_exit_clients(&clients_list, &conn);
-        if res.is_err() {
-            error!("Exit client cleanup failed with {:?}", res);
-        }
-
-        // Make sure no one we are setting up is geoip unauthorized
-        if !SETTING.get_allowed_countries().is_empty() {
-            Arbiter::spawn(validate_clients_region(clients_list.clone()));
-        }
-
-        // handle enforcement on client tunnels by querying debt keeper
-        // this consumes client list, you can move it up in exchange for a clone
-        Arbiter::spawn(enforce_exit_clients(clients_list));
-
-        info!(
-            "Completed Rita sync loop in {}s {}ms, all vars should be dropped",
-            start.elapsed().as_secs(),
-            start.elapsed().subsec_millis(),
-        );
-        Ok(())
-    }
+    });
 }
 
 fn setup_exit_wg_tunnel() {
@@ -198,7 +142,7 @@ fn setup_exit_wg_tunnel() {
 }
 
 pub fn check_rita_exit_actors() {
-    assert!(crate::rita_exit::rita_loop::RitaLoop::from_registry().connected());
+    start_rita_exit_loop();
     assert!(crate::rita_exit::traffic_watcher::TrafficWatcher::from_registry().connected());
     assert!(crate::rita_exit::database::db_client::DbClient::from_registry().connected());
 }
