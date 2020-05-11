cb266d92c042e1c0fdd15474f6bfc66a965817c4
Justin Kilpatrick <justin@althea.net>
2020-05-11 19:20:00 -0400

    Bump for Beta 14 RC1

--- rita/src/rita_common/dashboard/own_info.rs
+++ rita/src/rita_common/dashboard/own_info.rs
@@ -6,7 +6,7 @@ use failure::Error;
 use num256::{Int256, Uint256};
 use settings::RitaCommonSettings;
 
-pub static READABLE_VERSION: &str = "Beta 13 RC7";
+pub static READABLE_VERSION: &str = "Beta 14 RC1";
 
 #[derive(Serialize)]
 pub struct OwnInfo {
