77a46506c19ecaafeafc7c90b3f3b91b1d658d4b
Justin Kilpatrick <justin@althea.net>
2020-05-28 12:05:20 -0400

    No Wyre preface message
    Can't figure out how to make this look good, so it goes for now

--- rita/src/rita_client/dashboard/localization.rs
+++ rita/src/rita_client/dashboard/localization.rs
@@ -8,7 +8,6 @@ use settings::{localization::LocalizationSettings, RitaCommonSettings};
 pub struct LocalizationReturn {
     pub wyre_enabled: bool,
     pub wyre_account_id: String,
-    pub wyre_preface_message: String,
     pub display_currency_symbol: bool,
     pub support_number: String,
 }
@@ -18,7 +17,6 @@ impl From<LocalizationSettings> for LocalizationReturn {
         LocalizationReturn {
             wyre_enabled: input.wyre_enabled,
             wyre_account_id: input.wyre_account_id,
-            wyre_preface_message: input.wyre_preface_message,
             display_currency_symbol: input.display_currency_symbol,
             support_number: input.support_number.to_string(),
         }
--- settings/src/localization.rs
+++ settings/src/localization.rs
@@ -16,13 +16,6 @@ fn default_support_number() -> PhoneNumber {
     "+18664ALTHEA".parse().unwrap()
 }
 
-fn default_wyre_preface_message() -> String {
-    "Our payment partner Wyre, is international and expects phone numbers in international format. \
-    The United States country code is +1 followed by your area code and number. \
-    You may also see the charge come from outside the United States, this is normal."
-        .to_string()
-}
-
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub struct LocalizationSettings {
     /// A flag indicating whether or not the dashboard should give users the option to purchase
@@ -32,11 +25,6 @@ pub struct LocalizationSettings {
     /// Wyre account_id used to associate transactions with a specific Wyre account
     #[serde(default = "default_wyre_account_id")]
     pub wyre_account_id: String,
-    /// This is a message that prefaces the Wyre deposit to include some warnings for the
-    /// user. Wyre often changes their flow or introduces pitfalls for users. We sadly need
-    /// to be able to modify our prep message
-    #[serde(default = "default_wyre_preface_message")]
-    pub wyre_preface_message: String,
     /// If we should display the $ symbol or just the DAI star symbol next
     /// to the balance, designed to help manage how prominently we want the cryptocurrency
     /// aspect of Althea to be displayed to the user.
@@ -54,7 +42,6 @@ impl Default for LocalizationSettings {
         LocalizationSettings {
             wyre_enabled: default_wyre_enabled(),
             wyre_account_id: default_wyre_account_id(),
-            wyre_preface_message: default_wyre_preface_message(),
             display_currency_symbol: default_display_currency_symbol(),
             support_number: default_support_number(),
         }
