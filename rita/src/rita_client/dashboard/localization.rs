0a61cc903a763028749b910c323b4bc632a54070
Justin Kilpatrick <justin@althea.net>
2020-08-03 17:41:56 -0400

    Remove Wyre account ID
    This is now held in the reservation flow, so it can be removed

--- rita/src/rita_client/dashboard/localization.rs
+++ rita/src/rita_client/dashboard/localization.rs
@@ -18,7 +18,6 @@ use std::time::Duration;
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub struct LocalizationReturn {
     pub wyre_enabled: bool,
-    pub wyre_account_id: String,
     pub display_currency_symbol: bool,
     pub support_number: String,
 }
@@ -27,7 +26,6 @@ impl From<LocalizationSettings> for LocalizationReturn {
     fn from(input: LocalizationSettings) -> Self {
         LocalizationReturn {
             wyre_enabled: input.wyre_enabled,
-            wyre_account_id: input.wyre_account_id,
             display_currency_symbol: input.display_currency_symbol,
             support_number: input
                 .support_number
--- settings/src/localization.rs
+++ settings/src/localization.rs
@@ -4,10 +4,6 @@ fn default_wyre_enabled() -> bool {
     true
 }
 
-fn default_wyre_account_id() -> String {
-    "AC_2J6LWQEGW8P".to_string()
-}
-
 fn default_display_currency_symbol() -> bool {
     true
 }
@@ -22,9 +18,6 @@ pub struct LocalizationSettings {
     /// cryptocurrency through Wyre as part of the funding flow.
     #[serde(default = "default_wyre_enabled")]
     pub wyre_enabled: bool,
-    /// Wyre account_id used to associate transactions with a specific Wyre account
-    #[serde(default = "default_wyre_account_id")]
-    pub wyre_account_id: String,
     /// If we should display the $ symbol or just the DAI star symbol next
     /// to the balance, designed to help manage how prominently we want the cryptocurrency
     /// aspect of Althea to be displayed to the user.
@@ -41,7 +34,6 @@ impl Default for LocalizationSettings {
     fn default() -> LocalizationSettings {
         LocalizationSettings {
             wyre_enabled: default_wyre_enabled(),
-            wyre_account_id: default_wyre_account_id(),
             display_currency_symbol: default_display_currency_symbol(),
             support_number: default_support_number(),
         }
