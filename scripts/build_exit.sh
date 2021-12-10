00a2772b82d3740d0cab44cf1a37da3bf1e8d178
Thomas Spellman <thomas@althea.systems>
2021-12-10 07:24:58 -0800

    Exit: improve build script

--- scripts/build_exit.sh
+++ scripts/build_exit.sh
@@ -1,6 +1,12 @@
-cd cross-builders/exit
+#!/bin/bash
+set -eux
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
+
+pushd $DIR/../cross-builders/exit
 docker build -t cross-with-clang-ssl .
 cp Cross.toml ../..
-cd ../..
+popd
+pushd $DIR/..
 cross build --release --target x86_64-unknown-linux-gnu  -p rita_bin --bin rita_exit
 rm Cross.toml
