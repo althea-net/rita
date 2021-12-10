a069c84497b97f208c90a4f29caa6aa2d45155ae
Thomas Spellman <thomas@althea.systems>
2021-12-09 18:47:08 -0800

    Exit: Add simple cross builder script
    Uses a docker container to provide necessary deps to cross build rita-exit

---
+++ scripts/build_exit.sh
@@ -0,0 +1,6 @@
+cd cross-builders/exit
+docker build -t cross-with-clang-ssl .
+cp Cross.toml ../..
+cd ../..
+cross build --release --target x86_64-unknown-linux-gnu  -p rita_bin --bin rita_exit
+rm Cross.toml
