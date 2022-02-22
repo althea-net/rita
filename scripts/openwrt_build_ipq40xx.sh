1a08b5657961a86ed1bf2e552965f66b8d7b2be9
Chiara Seim <chiara@althea.systems>
2022-02-22 14:44:01 -0800

    Migrates openwrt_build scripts to use cross which maintains the openwrt toolchain

--- scripts/openwrt_build_ipq40xx.sh
+++ scripts/openwrt_build_ipq40xx.sh
@@ -5,23 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Parse command line arguments
 source $DIR/build_common.sh
 
-if [[ ! -d $DIR/staging_dir ]]; then
-    pushd $DIR
-    wget -N https://updates.altheamesh.com/staging.tar.xz -O staging.tar.xz > /dev/null; tar -xf staging.tar.xz
-fi
+cargo install cross
 
-
-
-export TOOLCHAIN=toolchain-arm_cortex-a7+neon-vfpv4_gcc-7.3.0_musl_eabi
-export TARGET_CC=$DIR/staging_dir/$TOOLCHAIN/bin/arm-openwrt-linux-gcc
-export TARGET_LD=$DIR/staging_dir/$TOOLCHAIN/bin/arm-openwrt-linux-ld
-export TARGET_AR=$DIR/staging_dir/$TOOLCHAIN/bin/arm-openwrt-linux-ar
-export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_LINKER=$TARGET_CC
-export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_AR=$TARGET_AR
-export SQLITE3_LIB_DIR=$DIR/staging_dir/target-arm_cortex-a7+neon-vfpv4_musl_eabi/usr/lib/
-export ARMV7_UNKNOWN_LINUX_MUSLEABIHF_OPENSSL_DIR=$DIR/staging_dir/target-arm_cortex-a7+neon-vfpv4_musl_eabi/usr/
-export OPENSSL_STATIC=1
-
-rustup target add armv7-unknown-linux-musleabihf
-
-cargo build --target armv7-unknown-linux-musleabihf ${PROFILE} ${FEATURES} -p rita_bin --bin rita
+cross build --target armv7-unknown-linux-musleabihf ${PROFILE} ${FEATURES} -p rita_bin --bin rita --features rita_bin/bundle_openssl
