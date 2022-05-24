00d460751ac433a343b4b2b8315cc3c3b5273644
Justin Kilpatrick <justin@althea.net>
2022-05-24 06:56:57 -0400

    Update exit builder script
    Debian builds where not compatible with centos, so this patch
    successfully gets us building with cross gnu

--- scripts/build_exit.sh
+++ scripts/build_exit.sh
@@ -4,5 +4,5 @@ cd cross-builders/exit
 docker build -t cross-with-clang-ssl .
 cp Cross.toml ../..
 cd ../..
-cross build --release --target x86_64-unknown-linux-gnu  -p rita_bin --bin rita_exit
+cross build --release --target x86_64-unknown-linux-gnu -p rita_bin --bin rita_exit
 rm Cross.toml 
