295c9463fbb1fea83b893e1d33c030c3302faa20
Justin Kilpatrick <justin@althea.net>
2022-05-27 18:12:13 -0400

    Bump for Beta19 RC10

--- rita_common/src/dashboard/own_info.rs
+++ rita_common/src/dashboard/own_info.rs
@@ -6,7 +6,7 @@ use actix_web_async::HttpResponse;
 use clarity::Address;
 use num256::{Int256, Uint256};
 
-pub static READABLE_VERSION: &str = "Beta 19 RC9";
+pub static READABLE_VERSION: &str = "Beta 19 RC10";
 
 #[derive(Serialize)]
 pub struct OwnInfo {
