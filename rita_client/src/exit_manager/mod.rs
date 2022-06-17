4751080359b076978e696160902bc7e92746f6cd
Pranay Tulugu <pranay@althea.systems>
2022-06-17 09:44:23 -0700

    Rustfmt and Clippy

--- rita_client/src/exit_manager/mod.rs
+++ rita_client/src/exit_manager/mod.rs
@@ -146,7 +146,7 @@ fn linux_setup_exit_tunnel(
 
     let args = ClientExitTunnelConfig {
         endpoint: SocketAddr::new(
-            get_selected_exit(exit.clone()).expect("There should be an exit ip here"),
+            get_selected_exit(exit).expect("There should be an exit ip here"),
             general_details.wg_exit_port,
         ),
         pubkey: current_exit.wg_public_key,
