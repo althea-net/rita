df5d5d4174b2aef760e152eaaa2b254a6d4811a2
Pranay Tulugu <pranay@althea.systems>
2022-09-29 13:53:26 -0700

    Fix route setup bug.

--- scripts/openwrt_build_ipq40xx.sh
+++ scripts/openwrt_build_ipq40xx.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Parse command line arguments
 source $DIR/build_common.sh
 
-cargo install cross
+# cargo install cross
 
 cross build --target armv7-unknown-linux-musleabihf ${PROFILE} ${FEATURES} -p rita_bin --bin rita
