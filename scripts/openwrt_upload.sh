66fd5d31dd07e5a4c5f012e0742e2a94ef583bf8
Justin Kilpatrick <justin@althea.net>
2023-03-21 10:02:56 -0400

    Fixup openwrt upload
    Some local changes had snuck into this file, this patch reverts them and
    also ensures the default setup is useful for dash development including
    the local port forwarding required.

--- scripts/openwrt_upload.sh
+++ scripts/openwrt_upload.sh
@@ -2,10 +2,10 @@
 set -eux
 export TARGET=ipq40xx
 export TRIPLE=armv7-unknown-linux-musleabihf
-export ROUTER_IP=test-hap
-bash scripts/openwrt_build_$TARGET.sh
+export ROUTER_IP=192.168.10.1
+bash scripts/openwrt_build_$TARGET.sh --features rita_bin/development
 set +e
 ssh root@$ROUTER_IP killall -9 rita
 set -e
 scp -O target/$TRIPLE/release/rita root@$ROUTER_IP:/tmp/rita
-ssh root@$ROUTER_IP NO_REMOTE_LOG=TRUE RUST_BACKTRACE=FULL RUST_LOG=TRACE /tmp/rita --config=/etc/rita.toml --platform=linux &> out.log
+ssh -L localhost:4877:localhost:4877 root@$ROUTER_IP NO_REMOTE_LOG=TRUE RUST_BACKTRACE=FULL RUST_LOG=TRACE /tmp/rita --config=/etc/rita.toml --platform=linux &> out.log
