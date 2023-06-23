4d935d24ddd4a3c5eb996bfec0a51b0fc4621929
Justin Kilpatrick <justin@althea.net>
2023-06-23 12:40:08 -0400

    Move test_runner into integration_tests
    The goal of this change is to make it possible ot import the test code
    downstream as a library and easily reuse it elsewhere to test downstream
    code.

---
+++ integration_tests/src/lib.rs
@@ -0,0 +1,10 @@
+use std::time::Duration;
+
+pub mod five_nodes;
+pub mod payments_althea;
+pub mod payments_eth;
+pub mod setup_utils;
+pub mod utils;
+
+/// The amount of time we wait for a network to stabalize before testing
+pub const SETUP_WAIT: Duration = Duration::from_secs(60);
