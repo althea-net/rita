e7ee0005ed863cce38465354548021c496623810
Christian Borst <christianoborst@gmail.com>
2023-08-18 09:24:49 -0400

    Add base solidity directory for Althea-L1 contracts
    The solidity folder functions as a solidity development environment
    under Rita, with functions to write + unit test + compile solidity
    contracts for deployment on the Althea-L1 EVM.
    
    This folder is a simpler version of the one in
    github.com/althea-net/althea-L1. For contracts to be available on the
    Althea-L1 EVM in integration tests, the testing startup process can
    execute solidity/scripts/contract-deployer.sh
    
    See the README.md for lots of info and the commented files for
    explanations of how to deploy and test new contracts.

---
+++ solidity/test-utils/index.ts
@@ -0,0 +1,19 @@
+import { TestERC20A } from "../typechain/TestERC20A";
+import { TestERC20B } from "../typechain/TestERC20B";
+import { TestERC20C } from "../typechain/TestERC20C";
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+
+export async function deployContracts(signer?: Signer | undefined) {
+
+  const TestERC20A = await ethers.getContractFactory("TestERC20A", signer);
+  const testERC20A = (await TestERC20A.deploy()) as TestERC20A;
+
+  const TestERC20B = await ethers.getContractFactory("TestERC20B", signer);
+  const testERC20B = (await TestERC20B.deploy()) as TestERC20B;
+
+  const TestERC20C = await ethers.getContractFactory("TestERC20C", signer);
+  const testERC20C = (await TestERC20C.deploy()) as TestERC20C;
+
+  return { testERC20A, testERC20B, testERC20C };
+}
