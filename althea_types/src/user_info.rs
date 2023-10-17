a790db738f82c0cb00002af041816f46e29176d6
Justin Kilpatrick <justin@althea.net>
2023-10-17 08:41:33 -0400

    Fix sort order for flat usage data
    I believed we needed to reverse the sort order from the default greatest to least to least to greatest for frontend display on the operator tools side, but that was actually already taken care of.

--- althea_types/src/user_info.rs
+++ althea_types/src/user_info.rs
@@ -173,6 +173,6 @@ pub fn convert_map_to_flat_usage_data(input: HashMap<u64, Usage>) -> VecDeque<In
         })
     }
     // we want this sorted from greatest to least so we do the cmp in reverse order
-    out.make_contiguous().sort_by(|a, b| b.index.cmp(&a.index));
+    out.make_contiguous().sort_by(|a, b| a.index.cmp(&b.index));
     out
 }
