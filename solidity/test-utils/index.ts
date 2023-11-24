5c78aae7c9a51e4397a98a5ca868e6e404a3d365
Justin Kilpatrick <justin@althea.net>
2023-11-24 15:28:46 +0530

    Clean up redundant references to test erc20 contracts

--- solidity/test-utils/index.ts
+++ solidity/test-utils/index.ts
@@ -7,17 +7,8 @@ import { Signer } from "ethers";
 
 export async function deployContracts(signer?: Signer | undefined) {
 
-  const TestERC20A = await ethers.getContractFactory("TestERC20A", signer);
-  const testERC20A = (await TestERC20A.deploy()) as TestERC20A;
-
-  const TestERC20B = await ethers.getContractFactory("TestERC20B", signer);
-  const testERC20B = (await TestERC20B.deploy()) as TestERC20B;
-
-  const TestERC20C = await ethers.getContractFactory("TestERC20C", signer);
-  const testERC20C = (await TestERC20C.deploy()) as TestERC20C;
-
   const AltheaDB = await ethers.getContractFactory("AltheaDB", signer);
   const althea_db = (await AltheaDB.deploy(signer?.getAddress())) as AltheaDB;
 
-  return { testERC20A, testERC20B, testERC20C, althea_db };
+  return { althea_db };
 }
