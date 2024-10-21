7337dcbb77ebdc5acab321eb4a331c47b29d957a
Justin Kilpatrick <justin@althea.net>
2024-10-21 09:31:06 -0400

    Bump for Beta 22 RC4

--- rita_common/src/dashboard/own_info.rs
+++ rita_common/src/dashboard/own_info.rs
@@ -7,7 +7,7 @@ use actix_web_async::HttpResponse;
 use clarity::Address;
 use num256::{Int256, Uint256};
 
-pub static READABLE_VERSION: &str = "Beta 22 RC3";
+pub static READABLE_VERSION: &str = "Beta 22 RC4";
 
 #[derive(Serialize)]
 pub struct OwnInfo {
